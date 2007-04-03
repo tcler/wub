e0585446ad6cd4a011c05d2f02a022adb59c9cc8
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-04-03 13:59:51 +0000

    initial import

---
+++ stx/stx.tcl
@@ -0,0 +1,341 @@
+package provide stx 1.1
+package require struct::tree
+
+namespace eval stx {
+    variable path	;# current path in tree
+    variable cursor root	;# current node in tree
+    variable tree ""	;# tree of list structure
+    variable tagnum 0	;# tag counter - a real number to provide disjoint sets
+    variable refs	;# set of all refs in text
+
+    # mapping from formatting prefix character to implementation
+    variable funct
+    array set funct {
+	" " pre
+	* li
+	# li
+	; dlist
+	= header
+	> indent
+	- hr
+	. special
+	\x82 li
+	\x83 li
+    }
+    variable state {}
+}
+
+# count the number of characters in the set 'leadin'
+proc stx::count {para leadin} {
+    set content [string trimleft $para $leadin]
+    return [expr {[string length $para] - [string length $content]}]
+}
+
+# tree debugging - generate the node path to root
+proc stx::path {{node ""}} {
+    variable tree
+    if {$node eq ""} {
+	variable cursor
+	set node $cursor
+    }
+    if {$node eq "root"} {
+	return root
+    } else {
+	return "[path [$tree parent $node]] $node/[$tree get $node type]"
+    }
+}
+
+# tree debugging - dump node
+proc stx::node {{node root}} {
+    variable tree
+    set result ""
+    if {$node eq ""} {
+	set node root
+    }
+
+    $tree walk $node n {
+	append result "[string repeat > [$tree depth $n]] [$tree get $n type]: "
+	foreach k [$tree keys $n] {
+	    if {$k ne "type"} {
+		append result " $k \"[$tree get $n $k]\""
+	    }
+	}
+	append result \n
+    }
+    return $result
+}
+
+# unwind the path stack to given new level of list nesting
+proc stx::unlist {{new {}}} {
+    variable path
+    if {$path ne $new} {
+	variable tree
+	variable cursor
+
+	# calculate the size of matching prefix
+	set match 0
+	foreach curr $path n $new {
+	    if {$curr ne $n} break
+	    incr match
+	}
+
+	# get the changes: undo set, then add set
+	set undo [lrange $path $match end]
+	set add [lrange $new $match end]
+	#puts stderr "Path: $path New: $new / $match / Undo: $undo Add: $add"
+
+	# unwind the cursor, moving up the tree
+	while {[llength $undo]} {
+	    set cursor [$tree parent $cursor]
+	    set undo [lrange $undo 0 end-1]
+	}
+
+	# construct list elements downward
+	foreach l $add {
+	    set cursor [$tree insert $cursor end]
+	    $tree set $cursor type $l
+	}
+
+	set path $new
+    }
+}
+
+# balance paired character-level markup, e.g. '' pairs
+proc stx::balance {text sep fn} {
+    set result ""
+    foreach {pre seg} [split [string map [list $sep \x81] $text] \x81] {
+	if {$seg ne ""} {
+	    append result "${pre}\[$fn ${seg}\]"
+	} else {
+	    append result $pre
+	}
+    }
+    return $result
+}
+
+# preprocess character-level markup
+proc stx::char {text} {
+    set text [string map {
+	\[\[ \x84
+	\]\] \x85
+	\; \x87
+    } $text]
+    # handle naked http references
+    regsub -all {([^[]|^)(http:[^ ]+)} $text {\1[http:\2]} text
+    #puts stderr "Char: '$text'"
+    variable refs
+	#puts stderr "ENCODING REFS: $text"
+    while {[regexp -- {\[[^]]+\]} $text ref]} {
+	set index [array size refs]
+	#puts stderr "ENCODE REF: $index $ref"
+	set refs($index) [string trim $ref {[]}]
+	regsub -- {\[[^]]+\]} $text "(\x86$index)" text
+    }
+
+    set text [balance $text ''' italic]
+    set text [balance $text '' strong]
+    set text [balance $text __ underline]
+    set text [balance $text %% smallcaps]
+    set text [balance $text -- strike]
+    set text [balance $text ^^^ subscript]
+    set text [balance $text ^^ superscript]
+    set text [balance $text !! big]
+
+    return $text	;# the list acts as a quote
+}
+
+# create a new node of given type with value at the given path
+proc stx::newnode {type {p {}}} {
+    variable tree
+    variable cursor
+    unlist $p
+    set node [$tree insert $cursor end]
+    $tree set $node type $type
+    #variable path
+    #$tree set $node path $path
+    return $node
+}
+
+proc stx::nodevalue {node val} {
+    variable tree
+    $tree set $node val [list $val]
+}
+
+proc stx::nodecdata {node val} {
+    variable tree
+    set cdata [$tree insert $node end]
+    $tree set $cdata type cdata
+    $tree set $cdata val [list [list $val]]
+}
+
+# identity preprocess for normal
+proc stx::normal {para} {
+    nodecdata [newnode normal] [char $para]
+}
+
+proc stx::special {para} {
+    nodecdata [newnode special] [string range $para 1 end]
+}
+
+# identity preprocess for hr
+proc stx::hr {args} {
+    newnode hr
+}
+
+# identity preprocess for pre
+proc stx::pre {para} {
+    nodecdata [newnode pre] [string range [string map {"\n " "\n"} $para] 1 end]
+}
+
+# preprocess for indent elements
+proc stx::indent {para} {
+    variable path
+    nodecdata [newnode indent $path] [char $para]
+}
+
+# preprocess for header elements
+proc stx::header {para} {
+    #puts stderr "HEADER: $para"
+    set count [count $para =]	;# depth of header nesting
+    set para [string trimleft $para =] ;# strip leading ='s
+
+    # we have the depth, and the start of the header
+    # now we need to find the end of header.
+    set p [split $para =]
+    set para [lindex $p 0]
+    set rest [string trimleft [join [lrange $p 1 end] =] "= "] ;# this may be a para
+
+    set para [split $para "\#"]
+    set tag [string trim [lindex $para end]]
+    set para [string trim [lindex $para 0]]
+
+    if {$tag eq ""} {
+	variable tagnum
+	set tagnum [expr $tagnum + 1.0] ;# allow non-int tagnums
+	set tag "h$tagnum"
+    }
+    set hnode [newnode header]
+    nodecdata $hnode $count
+    nodecdata $hnode $para
+    nodecdata $hnode $tag
+
+    # now process any pendant para
+    if {$rest ne ""} {
+	do_para $rest
+    }
+}
+
+# preprocess for list elements
+proc stx::li {para} {
+    set count [count $para "\#*"]	;# how many list levels deep?
+    puts stderr "li $count '$para'"
+    set li [string range $para 0 [expr {$count - 1}]]	;# list prefix
+    set li [split [string trim [string map {\# "ol " * "ul "} $li]]]
+    set para [string trim [string range $para $count end]]	;# content
+
+    nodecdata [newnode li $li] [char $para]
+    #nodecdata [newnode li $li] [do_para $para]
+}
+
+# preprocess for dlist elements
+proc stx::dlist {para {cpath ""}} {
+    set pp [split [char [string trimleft $para ";"]] :]
+    set term [lindex $pp 0]
+    set def [string trim [join [lrange $pp 1 end] ": "]]
+    set dlnode [newnode dl dlist]
+    #puts stderr "DLIST: '$para' - '[char $para]' - [char $term]"
+    nodecdata $dlnode $term	;#OR: nodecdata $dlnode $term ?
+    nodecdata $dlnode $def
+}
+
+proc stx::do_para {para} {
+    if {$para eq {}} return
+
+    set first [string index $para 0]
+    variable funct
+    if {[info exists funct($first)]} {
+	set f $funct($first)
+    } else {
+	set f normal
+    }
+    
+    puts stderr "STX: $f '$para'"
+    $f $para
+}
+
+# translate structured text into
+# tcl function calls over paragraphs
+proc stx::translate {text {tagstart 0}} {
+    # construct an empty tree
+    variable tree
+    if {$tree ne ""} {
+	$tree destroy
+    }
+    set tree [::struct::tree stx]
+    $tree set root type root
+
+    # our original path is empty
+    variable path {}
+    variable cursor root
+    variable tagnum $tagstart
+
+    set result ""
+    foreach para [split [string map {
+	\n# \x81\#
+	\n* \x81*
+	\n; \x81;
+	\n- \x81-
+	\n= \x81=
+	\n> \x81>
+
+	\n\n\n\n\n \x81
+	\n\n\n\n \x81
+	\n\n\n \x81
+	\n\n \x81
+
+	"\n " "\n "
+	\n " "
+	
+	\{ "\x89"
+	\} "\x8A"
+	$ "\x8B"
+	< "&lt\x87"
+	> "&gt\x87"
+    } $text] \x81] {
+	do_para $para
+    }
+
+    set result ""
+    variable tree
+    $tree walk root -order both {action node} {
+	if {$node eq "root"} continue
+	if {$action eq "enter"} {
+	    if {[$tree depth $node] > 1} {
+		append result " "
+	    }
+	    append result "\[[$tree get $node type]"
+	    if {[$tree keyexists $node val]} {
+		append result " [join [$tree get $node val]]"
+	    }
+	} else {
+	    append result "] "
+	    if {[$tree depth $node] == 1} {
+		append result "\n"
+	    }
+	}
+    }
+
+    # substitute refs back in
+    #puts stderr "STX REFSUBBING"
+    variable refs
+    while {[regexp -- {[(]\x86([^)]+)[)]} $result index]} {
+	#puts stderr "STX REFSUB $index -> $refs([string trim $index \x86()])"
+	regsub -- {[(]\x86[^)]+[)]} $result "\[ref \{$refs([string trim $index \x86()])\}\]" result
+    }
+
+    catch {unset refs}
+    set refs() ""
+    unset refs()
+    #puts stderr "STX RESULT: ${result}"
+    return ${result}
+}
---
+++ stx/stx2html.tcl
@@ -0,0 +1,425 @@
+package provide stx2html 1.1
+package require stx
+
+namespace eval stx2html {
+    variable features
+    array set features {}
+
+    variable toc
+    array set toc {}
+
+    variable toccnt {}
+    variable tagstart 0	;# number to start tagging TOC sections
+
+    variable title ""	;# title of page
+
+    variable img_properties ""
+}
+
+proc stx2html::tagstart {what} {
+    variable tagstart $what
+}
+
+# redefine subst - we only ever want commands in this application
+proc stx2html::subst {what} {
+    #puts stderr "subst: $what"
+    return [::subst -nobackslashes -novariables $what]
+}
+
+proc stx2html::cdata {args} {
+    return [join $args]
+}
+
+proc stx2html::normal {para args} {
+    return "<p>[subst $para]</p>\n[join $args]"
+}
+
+proc stx2html::pre {para args} {
+    return "<pre>$para</pre>\n[join $args]\n"
+}
+
+proc stx2html::special {para args} {
+    set p [split $para]
+    set cmd [string tolower [lindex $p 0]]
+    #puts stderr "special: $cmd"
+    if {[llength [info commands "_$cmd"]] == 1} {
+	set para [_$cmd [lrange $p 1 end]]
+    } else {
+	set para "<p>[::stx::char $para]</p>"
+    }
+    return "$para\n[join $args]\n"
+}
+
+proc stx2html::_title {args} {
+    variable title [join $args]
+    return ""
+}
+
+proc stx2html::_message {args} {
+    puts stderr "Message: [join $args]"
+    return ""
+}
+
+proc stx2html::_comment {args} {
+    return "<!-- [join $args] -->\n"
+}
+
+proc stx2html::_notoc {args} {
+    variable features
+    set features(NOTOC) 1
+    return ""
+}
+
+proc stx2html::_toc {args} {
+    variable features
+    set features(TOC) 1
+    catch {unset features(NOTOC)}
+    return "\x81TOC\x82"
+}
+
+proc stx2html::header {level para tag args} {
+    variable toc
+    variable toccnt
+    while {[llength $toccnt] <= $level} {
+	lappend toccnt 0
+    }
+    set toccnt [lrange $toccnt 0 $level]
+
+    lset toccnt $level [expr [lindex $toccnt $level] + 1]
+    set toc([join [lrange $toccnt 1 $level] .]) [list $para $tag]
+
+    set p [subst $para]
+    variable title
+    if {$title eq ""} {
+	set title $p
+    }
+
+    return "<h$level id='$tag'>$p</h$level>\n[join $args]\n"
+}
+
+proc stx2html::hr {} {
+    return "<hr style='clear:both;'>"
+}
+
+proc stx2html::indent {para} {
+    return "<p>[subst $para]</p>"
+}
+
+proc stx2html::dlist {args} {
+    return "<dl>[join $args \n]</dl>\n"
+}
+
+proc stx2html::dl {term def} {
+    #puts stderr "DL: $term / $def"
+    return "<dt>[subst $term]</dt>\n<dd>[subst $def]</dd>\n"
+}
+
+# make list item
+proc stx2html::li {content args} {
+    puts "2HTML: '$content' '$args'"
+    return "[subst $content]\n[join $args]\n"
+}
+
+proc stx2html::llist {form content} {
+    set result "<${form}>"
+    set open 0
+    set cnt 0
+    foreach arg $content {
+	if {$cnt && [string match {<[ou]l>*} $arg]} {
+	    if {!$open} {
+		append result "\n<li>"
+	    }
+	    append result "${arg}"
+	    set open 1
+	} else {
+	    if {$open} {
+		append result "\n</li>"
+	    }
+	    append result "\n<li>${arg}"
+	    set open 1
+	}
+	incr cnt
+    }
+    if {$open} {
+	append result "\n</li>"
+    }
+    return "${result}\n</${form}>\n"
+}
+
+# translate unordered list
+proc stx2html::ul {args} {
+    return [llist ul $args]
+}
+
+proc stx2html::ol {args} {
+    return [llist ol $args]
+}
+
+# This is a NOOP to convert local references to HTML
+# the application should supply its own version to the translate call
+proc stx2html::local {what} {
+    # org code
+    set what [split $what]
+    return "<a href='[lindex $what 0]'>[join [lrange $what 1 end]]</a>"
+
+    return $what
+
+    # org code
+    set what [split $what]
+    return "<a href='[lindex $what 0]'>[join [lrange $what 1 end]]</a>"
+}
+
+# make reference content
+proc stx2html::ref {what} {
+    set proto [split $what :]
+    set body [join [lrange $proto 1 end] :]
+    set proto [lindex $proto 0]
+    #puts "ref '$proto' '$body'"
+    switch -- $proto {
+	http {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {$text eq ""} {
+		set text "http:$body"
+	    }
+	    if {![string match /* $body]} {
+		set what "<a href='$body'>$text</a>"
+	    } else {
+		set what "<a href='http:$body'>$text</a>"
+	    }
+	}
+
+	acronym {
+	    set body [split $body "|"]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    set what "<acronym title='$text'>$body</acronym>"
+	}
+
+	\# {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    set what "<a name='$body'>$text</a>"
+	}
+
+	image {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {[string match /* $body]} {
+		set body "http:$body"
+	    }
+	    if {$text eq ""} {
+		set text $body
+	    }
+	    variable img_properties
+	    set what "<img src='$body' class='image' $img_properties alt='$text' />"
+	}
+
+	left {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {[string match /* $body]} {
+		set body "http:$body"
+	    }
+	    if {$text eq ""} {
+		set text $body
+	    }
+	    variable img_properties
+	    set what "<img src='$body' class='imageleft' $img_properties style='float:left' alt='$text' />"
+	}
+
+	right {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {[string match /* $body]} {
+		set body "http:$body"
+	    }
+	    if {$text eq ""} {
+		set text $body
+	    }
+	    variable img_properties
+	    set what "<img src='$body' class='imageright' $img_properties style='float:right' alt='$text' />"
+	}
+
+	default {
+	    variable local
+	    set what "<a href='[$local $what]'>"
+	}
+    }
+
+    return $what
+}
+
+# make reference content - original/old version
+proc stx2html::ref_org {what} {
+    if {[llength [set proto [split $what :]]] > 1} {
+	set body [join [lrange $proto 1 end] :]
+	set proto [lindex $proto 0]
+    } elseif {[string match \#* $what]} {
+	set proto http
+	set body $what
+    } else {
+	set proto ""
+    }
+    puts stderr "ref '$proto' '$body'"
+    switch -- $proto {
+	http {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {$text eq ""} {
+		set text "http:$body"
+	    }
+	    #puts stderr "http:  ... $body"
+	    if {![string match /* $body]} {
+		set what "<a href='$body'>$text</a>"
+	    } else {
+		set what "<a href='http:$body'>$text</a>"
+	    }
+	}
+
+	image {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {$text eq ""} {
+		set text "http:$body"
+	    }
+	    set what "<img src='http:$body' alt='$text'>"
+	}
+
+	left {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {$text eq ""} {
+		set text "http:$body"
+	    }
+	    set what "<img src='http:$body' style='float:left' alt='$text'>"
+	}
+
+	right {
+	    set body [split $body]
+	    set text [string trim [join [lrange $body 1 end]]]
+	    set body [string trim [lindex $body 0]]
+	    if {$text eq ""} {
+		set text "http:$body"
+	    }
+	    set what "<img src='http:$body' style='float:right' alt='$text'>"
+	}
+
+	default {
+	    variable local
+	    set what [$local $what]
+	}
+    }
+
+    return $what
+}
+
+# make content underlined
+proc stx2html::underline {args} {
+    return "<span style='text-decoration: underline;'>[join $args]</span>"
+}
+
+# make content underlined
+proc stx2html::strike {args} {
+    return "<span style='text-decoration: line-through;'>[join $args]</span>"
+}
+
+# make content subscript
+proc stx2html::subscript {args} {
+    return "<span style='vertical-align: sub;'>[join $args]</span>"
+}
+
+# make content superscript
+proc stx2html::superscript {args} {
+    return "<span style='vertical-align: super;'>[join $args]</span>"
+}
+
+# make content superscript
+proc stx2html::subscript {args} {
+    return "<span style='vertical-align: sub;'>[join $args]</span>"
+}
+
+# make content big
+proc stx2html::big {args} {
+    return "<span style='font-size: bigger;'>[join $args]</span>"
+}
+
+# make content strong
+proc stx2html::strong {args} {
+    return "<span style='font-weight: bold;'>[join $args]</span>"
+}
+
+# make content italic
+proc stx2html::italic {args} {
+    return "<span style='font-style: italic;'>[join $args]</span>"
+}
+
+# make content italic
+proc stx2html::smallcaps {args} {
+    return "<span style='font-variant: small-caps;'>[join $args]</span>"
+}
+
+proc stx2html::toc {} {
+    variable toc
+    set result "<table class='TOC'>\n"
+    append result "<thead><tr><td colspan='2'>Table Of Contents</td></tr></thead>"
+    append result "<tbody>"
+    foreach sect [lsort -dictionary [array names toc]] {
+	append result "<tr>"
+	append result "<td class='TOCnum'><a href='\#[lindex $toc($sect) 1]'>$sect</a></td> "
+	append result "<td>[lindex $toc($sect) 0]</td>\n"
+	append result "</tr>"
+    }
+    append result "</tbody>"
+    return "${result}</table>\n"
+}
+
+# convert structured text to html
+proc stx2html::translate {text {locallink ::stx2html::local}} {
+    variable features
+    array unset features
+    set features(NOTOC) 1
+    variable local $locallink
+
+    variable toc
+    array unset toc
+
+    variable title ""
+    variable toccnt {}
+    variable tagstart	;# number to start tagging TOC sections
+
+    set content [lindex [namespace inscope ::stx2html subst [list [stx::translate $text $tagstart]]] 0]
+
+    if {0} {
+	set content ""; set content1 ""; set content2 ""
+	if {[catch {stx::translate $text} content eo]} {
+	    return "<p>STX Error translate: '$text'<br>-> $eo</p>"
+	}
+	if {[catch {namespace inscope ::stx2html subst [list $content]} content1 eo]} {
+	    return "<p>STX Error subst: $content<br>-> $eo</p>"
+	}
+	if {[catch {lindex $content1 0} content eo]} {
+	    return "<p>STX Error lindex: '$content1'<br>-> $eo</p>"
+	}
+    }
+
+    if {![info exists features(NOTOC)]} {
+	if {[info exists features(TOC)]} {
+	    set content [string map [list "\x81TOC\x82" [toc]] $content]
+	} elseif {[array size toc] > 3} {
+	    set content "[toc]\n$content"
+	}
+    } else {
+	set content [string map [list "\x81TOC\x82" ""] $content]
+    }
+
+    return [string map {\x88 &\# \x89 \{ \x8A \} \x8B $ \x87 ; \x84 &#91; \x85 &#93; \\ ""} $content]
+}
