48aa111e940df773a46793aa562b5dfe60a9e16d
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-05-02 04:54:19 +0000

    * major rewrite - stronger error checking, better standards compliance, debugging. * [delete] was buggy, and would only delete the URL half of a cache key pair - leaving the etag half around, causing major havoc

--- Wub/Cache.tcl
+++ Wub/Cache.tcl
@@ -24,20 +24,24 @@ namespace eval Cache {
     variable unique 0
 
     proc exists? {key} {
+	if {$key eq ""} {return 0}	;# special case - no key
+
 	variable keys
-	set key [string trim $key \"]
+	set key [string trim $key \"]	;# remove ridiculous quotes
 	Debug.cache {exists: $key - [info exists keys($key)]}
 	return [info exists keys($key)]
     }
 
     proc invalidate {key} {
+	if {$key eq ""} return	;# special case - no key
+
 	Debug.cache {invalidate: $key}
 	variable keys
 	variable cache
-	Debug.cache {pre-invalidate: [array names keys]} 4
 
-	set key [string trim $key \"]
+	set key [string trim $key \"]	;# remove ridiculous quotes
 	if {[exists? $key]} {
+	    Debug.cache {invalidating $key in '[array names keys]'} 4
 	    set ckey $keys($key)
 	    if {[info exists cache($ckey)]} {
 		dict incr cache($ckey) -refcount -1
@@ -47,30 +51,26 @@ namespace eval Cache {
 	    }
 	    unset keys($key)
 	    Debug.cache {invalidated '$key'.}
+	    Debug.cache {post-invalidate: [array names keys]} 8
 	} else {
 	    Debug.cache {invalidate - no such element '$key'.}
 	}
-	Debug.cache {post-invalidate: [array names keys]} 4
     }
 
     proc delete {key} {
-	variable keys
 	Debug.cache {delete $key} 4
 	if {[exists? $key]} {
-	    set ckey $keys($key)
+	    variable keys
+	    set key [string trim $key \"]	;# remove ridiculous quotes
+	    set ckey $keys($key) ;# key under which the cached value is stored
+	    variable cache
 	    if {[info exists cache($ckey)]} {
 		Debug.cache {found cache: etag:[dict get? $cache($ckey) etag] url:[dict get? $cache($ckey) -url]}
-		set etag [dict get? $cache($ckey) etag]
-		if {$etag ne ""} {
-		    invalidate $etag
-		}
-
-		set url [dict get? $cache($ckey) -url]
-		if {$url ne ""} {
-		    invalidate $url
-		}
+		set cached $cache($ckey)
+		invalidate [dict get? $cached etag]
+		invalidate [dict get? $cached -url]
 	    }
-	    invalidate $key
+	    invalidate $key	;# remove offered key
 	}
     }
 
@@ -82,22 +82,34 @@ namespace eval Cache {
 	}
     }
 
+    proc fetch {req} {
+	Debug.cache {fetch: ([dumpMsg $req])}
+	variable keys
+	variable cache
+	if {[exists? [dict get? $req etag]]} {
+	    return $cache($keys([string trim [dict get $req etag] \"]))
+	} elseif {[exists? [dict get $req -url]]} {
+	    return $cache($keys([dict get $req -url]))
+	} else {
+	    error "Cache Fetching $req, no match."
+	}
+    }
+
     proc put {req} {
 	Debug.cache {put: ([dumpMsg $req])}
-	if {[exists? [dict get $req -url]]} {
-	    Debug.cache {putting url: [dict get $req -url]}
-	    # invalidate anything which matches this URL
-	    set cached [fetch $req]
-	    invalidate [dict get $cached etag]
-	    invalidate [dict get $cached -url]
+
+	# whatever the eventual cache status, must remove old matches
+	invalidate [dict get $req -url] ;# invalidate by -url
+	invalidate [dict get? $req etag] ;# invalidate by etag
+
+	# allow application to avoid caching by setting -dynamic
+	if {[dict get? $req -dynamic] ne ""} {
+	    return $req
 	}
 
 	if {[dict exists $req etag]} {
-	    # invalidate anything which matches this etag
-	    set etag [dict get $req etag]
-	    if {[exists? $etag]} {
-		invalidate $etag
-	    }
+	    set etag [string trim [dict get $req etag] \"]
+	    dict unset req etag	;# we don't want to store old etag - why?
 	} else {
 	    # generate an etag
 	    variable unique
@@ -105,137 +117,151 @@ namespace eval Cache {
 	    while {[exists? $etag]} {
 		set etag "[pid].[clock microseconds].[incr unique]"
 	    }
-	    dict set req etag \"[string trim $etag \"]\"
 	}
 
-	# allow application to avoid caching by setting -dynamic
-	if {[dict exists $req -dynamic]
-	    && [dict get $req -dynamic]
-	} {
-	    return $req
-	}
+	# subset the cacheable request with just those fields needed
+	set cached [dict subset $req -content -gzip -code -url]
+	set cached [dict merge $cached [dict subset $req {*}$::Http::rs_headers]]
 
-	set cached [dict subset $req {*}$::Http::rs_headers -content -gzip -code -url]
-	Debug.cache {stripped cache entry: $cached} 5
-	if {0} {
-	    # not strong enough
-	    foreach f {set-cookie -cookies -listener
-		-sock -method -transaction -ipaddr -worker
-	    } {
-		catch {dict unset cached $f}	;# do not cache field
-	    }
-	}
-
+	# add new fields for server cache control
 	dict set cached -refcount 2
 	dict set cached -when [clock seconds]
+	dict set cached etag \"$etag\"	;# store with ridiculous quotes
 
-	set etag [string trim $etag \"]
+	Debug.cache {cache entry: $cached} 4
 
-	# now insert req into cache
-	variable cache
-	set cache($etag) $cached
+	# insert cacheable request into cache under modified etag
+	variable cache; set cache($etag) $cached
 
-	# insert keys into key array
+	# insert keys into key array - match by -url or etag
 	variable keys
 	set keys($etag) $etag
 	set keys([dict get $req -url]) $etag
+
 	Debug.cache {new: $etag == [dict get $req -url]}
 
 	return $req
     }
 
-    proc keys {} {
+    # keys - return keys matching filter (default all)
+    proc keys {{filter {}}} {
 	variable keys
-	return [array names keys]
+	return [array names keys {*}$filter]
     }
 
     proc consistency {{fix 1}} {
 	variable keys
 	variable cache
-	foreach {name val} [array get keys] {
-	    if {$name eq $val} {
-		# etag key
-		if {![info exists cache($name)]} {
-		    Debug.error {etag key no matching cache $name}
-		    if {$fix} {
-			unset keys($name)
+	set check 1
+	while {check} {
+	    set check 0
+	    foreach {name val} [array get keys] {
+		if {$name eq $val} {
+		    # etag key
+		    if {![info exists cache($name)]} {
+			Debug.error {etag key no matching cache $name}
+			if {$fix} {
+			    unset keys($name)
+			    incr check
+			}
 		    }
-		}
-	    } else {
-		# url key
-		if {![info exists cache($val)]} {
-		    Debug.error {url key $name no matching cache $val}
-		    if {$fix} {
-			unset keys($val)
+		} else {
+		    # url key
+		    if {![info exists cache($val)]} {
+			Debug.error {url key $name no matching cache $val}
+			if {$fix} {
+			    unset keys($val)
+			    incr check
+			}
 		    }
 		}
 	    }
-	}
 
-	foreach {name val} [array get cache] {
-	    if {![info exists keys($name)]} {
-		# no etag key for cache
-		Debug.error {orphan cache $name / $cache($name)}
-	    } else {
+	    foreach {name val} [array get cache] {
+		if {[catch {
+		    if {![exists? keys($name)]} {
+			# no etag key for cache
+			error {orphan cache by name '$name' / $cache($name)}
+		    }
+		    if {![exists? [dict get $val -url]]} {
+			error {orphan cache by url '[dict get? $val -url]' / $name - '$cache($name)'}
+		    }
+		    if {![exists? [dict get $val etag]]} {
+			error {orphan cache by etag '[dict get? $val etag]' / $name - '$cache($name)'}
+		    }
+		    if {[string trim [dict get $val etag] \"] ne $name} {
+			error {etag and cache name mismatch}
+		    }
+		} r eo]} {
+		    debug.error {cache consistency: $eo}
+			if {$fix} {
+			    unset cache($name)
+			    incr check
+			}
+		}
 	    }
 	}
     }
 
-    proc fetch {req} {
-	Debug.cache {fetch: ([dumpMsg $req])}
-	variable keys
-	variable cache
-	if {[dict exists $req etag]
-	    && [info exists keys([dict get $req etag])]
-	} {
-	    return $cache($keys([dict get $req etag]))
-	} elseif {[info exists keys([dict get $req -url])]} {
-	    return $cache($keys([dict get $req -url]))
-	} else {
-	    error "Cache Fetching $req, no match."
-	}
-    }
+    variable CC 0	;# do we bother to parse cache-control?
+    variable obey_CC 0	;# do we act on cache-control? (Not Implemented)
 
     proc check {req} {
 	Debug.cache {check [dict get $req -url]: ([dumpMsg $req])}
 	# first query cache to see if there's even a matching entry
-	if {!([dict exists $req etag] && [exists? $etag])
-	    && ![exists? [dict get $req -url]]} {
-	    Debug.cache {no: neither etag and url are in cache}
-	    return {}	;# we don't have a copy
+	set etag [dict get? $req etag]
+	set url [dict get? $req -url]
+	if {$etag ne "" && ![exists? $etag]} {
+	    Debug.cache {etag '$etag' given, but not in cache}
+	    return {}	;# we don't have a copy matching etag
 	}
+	if {$url ne "" && ![exists? $url]} {
+	    Debug.cache {url '$url' not in cache}
+	    return {}	;# we don't have a copy matching -url either
+	}
+	dict set req etag $etag
 
 	# old style no-cache request
-	if {[dict exists $req pragma]
-	    && ("no-cache" in [split [dict get $req pragma] ,])
+	variable obey_CC
+	variable CC
+	if {$CC && 
+	    "no-cache" in [split [dict get? $req pragma] ,]
 	} {
 	    # ignore no-cache, because we're the server, and in the best
 	    # position to judge the freshness of our content.
 	    Debug.cache {no-cache requested - we're ignoring those!}
-	    # return {}
+	    if {$obey_CC} {return {}}
 	}
 
 	# split any cache control into an array
-	if {[dict exists $req -cache-control]} {
-	    set cacheable [split [dict get $req -cache-control] ,]
-	    foreach directive $cacheable {
+	if {$CC
+	    && [dict exists $req -cache-control]
+	} {
+	    foreach directive [split [dict get $req -cache-control] ,] {
 		set body [string trim [join [lassign [split $directive =] d] =]]
 		set d [string trim $d]
 		set cc($d) $body
 	    }
+	    Debug.cache {no-cache requested [array get cc]}
+
 	    if {[info exists cc(no-cache)]
 		|| ([info exists cc(max-age)] && ($cc(max-age)==0))} {
-		Debug.cache {no-cache requested [array get cc] - we're ignoring these}
-		#return {}	;# no cache.
+		if {$obey_CC} {return {}}	;# no cache.
 	    }
-	    if {[info exists cc(max-age)]} {
+
+	    if {$obey_CC && [info exists cc(max-age)]} {
 		# we ignore max_age
 		#set max_age [Http DateInSeconds $cc(max-age)]
 	    }
 	}
 
 	# we may respond from cache, we *do* have a cached copy
-	set cached [fetch $req]
+	if {[catch {set cached [fetch $req]} cached eo]} {
+	    # it's gotta be there!
+	    Debug.error {cache inconsistency - can't fetch existing entry for url:'$url' etag:'$etag'}
+	    return {}
+	}
+
 	if {[info exists max_age]
 	    && (([dict $cached -when] - [clock seconds]) > $max_age)
 	} {
@@ -272,19 +298,18 @@ namespace eval Cache {
 	}
 
 	if {[unmodified? $req $cached]} {
-	    Debug.cache {unmodified}
+	    Debug.cache {unmodified $url}
 	    return [Http NotModified $req]
+	    # NB: the expires field is set in $req
 	} else {
 	    # deliver cached content in lieue of processing
-	    Debug.cache {cached content}
-	    if {![dict exists $req last-modified]} {
-		dict set req last-modified [Http Date [dict get $cached -when]]
-	    }
+	    Debug.cache {cached content for $url}
+	    dict set req last-modified [Http Date [dict get $cached -when]]
 
 	    set req [dict merge $req $cached]
 	    set req [Http CacheableContent $req [dict get $cached -when]]
 	    if {[dict get $cached -code] eq 404} {
-		dict set req -code 404
+		dict set req -code 404	;# we can cache 404s
 	    }
 	    return $req
 	}
@@ -296,3 +321,5 @@ namespace eval Cache {
     namespace export -clear *
     namespace ensemble create -subcommands {}
 }
+
+Debug off cache 10	;# debug cache access decisions
