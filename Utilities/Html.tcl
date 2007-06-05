0a0c63227b25be9e474d89998c615396cb410ee8
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-06-05 01:15:21 +0000

    * added subst-capable command variants

--- Utilities/Html.tcl
+++ Utilities/Html.tcl
@@ -52,3 +52,56 @@ know {[string match <*> [lindex $args 0]]} {
     }]
     return [eval $args]
 }
+
+# Some command equivalents which use subst instead of eval
+
+# If: Subst-if
+# Note that this version does not support the then keyword in if,
+# and requires the else keyword.
+proc If {args} {
+    while {[llength $args] && ![uplevel expr [lindex $args 0]]} {
+	set args [lrange $args 2 end]	;# lose the cond and positive-cond
+	
+	if {[lindex $args 0] eq "else"} {
+            break
+        }
+	
+	set args [lrange $args 1 end] ;# assumed to be 'elseif'
+    }
+    return [uplevel subst [list [lindex $args 1]]] ;# return with neg-consequence
+}
+
+# Subst-while
+proc While {cond body} {
+    set result {}
+    while {[uplevel $cond]} {
+	lappend result [uplevel subst [list $body]]
+    }
+    return $result
+}
+
+# Subst-foreach
+proc Foreach {args} {
+    set body [lindex $args end]
+    set vars [lrange $args 0 end-1]
+    set script [string map [list %A $args %B $body %V $vars] {
+	foreach %V {
+	    lappend {%A} [subst {%B}]
+	}
+	return [set {%A}]
+    }]
+    return [uplevel $script]
+}
+
+# Subst-switch
+proc Switch {args} {
+    set switch {}
+    foreach {key body} [lindex $args end] {
+	if {$body eq "-"} {
+	    lappend switch $key -
+	} else {
+	    lappend switch $key [list subst $body]
+	}
+    }
+    return [uplevel [list switch {*}[lrange $args 0 end-1] $switch]]
+}
