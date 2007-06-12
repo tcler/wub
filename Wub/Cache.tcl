9b6bf73cdb397298c3ec3ad18fde3836c2f871ee
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-06-12 23:36:43 +0000

    * Cache mods to limit size and gather stats

--- Wub/Cache.tcl
+++ Wub/Cache.tcl
@@ -82,21 +82,61 @@ namespace eval Cache {
 	}
     }
 
+    # cache effectiveness stats
+    variable hits 0
+    variable attempts 0
+
+    # fetch - try to find an entry matching req
     proc fetch {req} {
 	Debug.cache {fetch: ([dumpMsg $req])}
+
 	variable keys
 	variable cache
 	if {[exists? [Dict get? $req etag]]} {
-	    return $cache($keys([string trim [dict get $req etag] \"]))
+	    set key $keys([string trim [dict get $req etag] \"])
 	} elseif {[exists? [dict get $req -url]]} {
-	    return $cache($keys([dict get $req -url]))
+	    set key $keys([dict get $req -url])
 	} else {
 	    error "Cache Fetching $req, no match."
 	}
+
+	# maintain some stats for cache management
+	variable hits; incr hits	;# count cache hits
+	dict incr cache($key) -hits	;# count individual entry hits
+
+	return $cache($key)
+    }
+
+    # high and low water mark for cache occupancy
+    variable high 100
+    variable low 90
+    variable weight_age 1.0
+    variable weight_hits -2.0
+
+    # stale_sort - return objects in staleness order
+    # staleness is a measure of #hits and age of entry
+    proc stale_sort {a b} {
+	variable cache;
+	variable weight_age; variable weight_hits
+
+	set ca $cache($a);
+	set hits_a [dict get $ca -hits]; set age_a [dict get $ca -when]
+	set weight_a [expr {($hits_a * $weight_hits) + ($age_a * $weight_age)}]
+
+	set cb $cache($b);
+	set hits_b [dict get $cb -hits]; set age_b [dict get $cb -when]
+	set weight_b [expr {($hits_b * $weight_hits) + ($age_b * $weight_age)}]
+
+	return [expr {int($weight_a - $weight_b)}]
     }
 
     proc put {req} {
 	Debug.cache {put: ([dumpMsg $req])}
+
+	# whatever the eventual cache status, must remove old matches
+	invalidate [dict get $req -url] ;# invalidate by -url
+	invalidate [Dict get? $req etag] ;# invalidate by etag
+
 	if {[dict get $req -code] != 200} {
 	    return $req
 	}
@@ -105,10 +145,6 @@ namespace eval Cache {
 	    || [string match */x-* $ctype]} {
 	    return $req
 	}
-
-	# whatever the eventual cache status, must remove old matches
-	invalidate [dict get $req -url] ;# invalidate by -url
-	invalidate [Dict get? $req etag] ;# invalidate by etag
 
 	# allow application to avoid caching by setting -dynamic
 	if {[Dict get? $req -dynamic] ne ""} {
@@ -139,11 +175,34 @@ namespace eval Cache {
 	dict set cached -refcount 2
 	dict set cached -when [clock seconds]
 	dict set cached etag \"$etag\"	;# store with ridiculous quotes
+	dict set cached -hits 0
 
 	Debug.cache {cache entry: $cached} 4
 
+	variable cache; variable high; variable low
+	# ensure cache size is bounded
+	set cachesize [array size cache]
+	if {$cachesize > $high} {
+	    set ordered [lsort -command ::Cache::stale_sort [array names cache]]
+	    while {$cachesize > $low} {
+		# pick a cache entry to remove by weight
+		set c [lindex $ordered 0]
+		set ordered [lrange $ordered 1 end]
+
+		# remove the selected entry
+		if {[catch {
+		    invalidate [dict get $cache($c) -url] ;# invalidate by -url
+		    invalidate [Dict get? $cache($c) etag] ;# invalidate by etag
+		}]} {
+		    break
+		}
+
+		incr cachesize -1
+	    }
+	}
+
 	# insert cacheable request into cache under modified etag
-	variable cache; set cache($etag) $cached
+	set cache($etag) $cached
 
 	# insert keys into key array - match by -url or etag
 	variable keys
@@ -161,6 +220,7 @@ namespace eval Cache {
 	return [array names keys {*}$filter]
     }
 
+    # consistency - check or ensure cache consistency
     proc consistency {{fix 1}} {
 	variable keys
 	variable cache
@@ -182,7 +242,7 @@ namespace eval Cache {
 		    if {![info exists cache($val)]} {
 			Debug.error {url key $name no matching cache $val}
 			if {$fix} {
-			    unset keys($val)
+			    catch {unset keys($val)}
 			    incr check
 			}
 		    }
@@ -218,8 +278,11 @@ namespace eval Cache {
     variable CC 0	;# do we bother to parse cache-control?
     variable obey_CC 0	;# do we act on cache-control? (Not Implemented)
 
+    # check - 
     proc check {req} {
 	Debug.cache {check [dict get $req -url]: ([dumpMsg $req])}
+	variable attempts; incr attempts	;# count cache attempts
+
 	# first query cache to see if there's even a matching entry
 	set etag [Dict get? $req etag]
 	set url [Dict get? $req -url]
@@ -246,9 +309,7 @@ namespace eval Cache {
 	}
 
 	# split any cache control into an array
-	if {$CC
-	    && [dict exists $req -cache-control]
-	} {
+	if {$CC && [dict exists $req -cache-control]} {
 	    foreach directive [split [dict get $req -cache-control] ,] {
 		set body [string trim [join [lassign [split $directive =] d] =]]
 		set d [string trim $d]
@@ -270,7 +331,7 @@ namespace eval Cache {
 	# we may respond from cache, we *do* have a cached copy
 	if {[catch {set cached [fetch $req]} cached eo]} {
 	    # it's gotta be there!
-	    Debug.error {cache inconsistency - can't fetch existing entry for url:'$url' etag:'$etag'}
+	    Debug.error {cache inconsistency - can't fetch existing entry for url:'$url' etag:'$etag' because '$cached' ($eo)}
 	    return {}
 	}
 
