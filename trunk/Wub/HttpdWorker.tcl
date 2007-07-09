b4b3d17ff12a4b48b480324222a0cf9eda563ccd
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-07-09 08:21:32 +0000

    * stopped using chunked for gzip - FF should handle it ok

--- trunk/Wub/HttpdWorker.tcl
+++ trunk/Wub/HttpdWorker.tcl
@@ -293,7 +293,7 @@ proc CE {reply} {
 
     # choose content encoding - but not for MSIE
     variable chunk_size
-    if {[dict get -ua id] ne "MSIE"
+    if {[dict get $reply -ua id] ne "MSIE"
 	&& [dict exists $reply accept-encoding]
 	&& ![dict exists $reply content-encoding]
     } {
