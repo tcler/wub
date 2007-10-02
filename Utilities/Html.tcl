2710621fd29bcfaba6a2d8e5bb2216dfe3d9c11f
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2007-10-02 14:47:38 +0000

    * added MS demoroniser

--- Utilities/Html.tcl
+++ Utilities/Html.tcl
@@ -1,7 +1,53 @@
 package provide Html 1.0
 
 interp alias {} armour {} string map [list & &amp\; < &lt\; > &gt\; \" &quot\; ' &\#39\;]
+
+# xmlarmour - remove characters offensive to xml
 interp alias {} xmlarmour {} string map [list & &amp\; < &lt\; > &gt\; \" &quot\; ' &\#39\; \x00 " " \x01 " " \x02 " " \x03 " " \x04 " " \x05 " " \x06 " " \x07 " " \x08 " " \x0B " " \x0C " " \x0E " " \x0F " " \x10 " " \x11 " " \x12 " " \x13 " " \x14 " " \x15 " " \x16 " " \x17 " " \x18 " " \x19 " " \x1A " " \x1B " " \x1C " " \x1D " " \x1E " " \x1F " " \x7F " "]
+
+# control_armour - remove control characters
+interp alias {} control_armour {} string map [list \x00 " " \x01 " " \x02 " " \x03 " " \x04 " " \x05 " " \x06 " " \x07 " " \x08 " " \x0B " " \x0C " " \x0E " " \x0F " " \x10 " " \x11 " " \x12 " " \x13 " " \x14 " " \x15 " " \x16 " " \x17 " " \x18 " " \x19 " " \x1A " " \x1B " " \x1C " " \x1D " " \x1E " " \x1F " " \x7F " "]
+
+# demoronizer - remove MS specials.
+proc demoronizer {} {
+    set result {}
+    foreach line [split  {
+	128 8364 # euro sign
+	130 8218 # single low-9 quotation mark
+	131  402 # latin small letter f with hook
+	132 8222 # double low-9 quotation mark
+	133 8230 # horizontal ellipsis
+	134 8224 # dagger
+	135 8225 # double dagger
+	136  710 # modifier letter circumflex accent
+	137 8240 # per mille sign
+	138  352 # latin capital letter s with caron
+	139 8249 # single left-pointing angle quotation mark
+	140  338 # latin capital ligature oe
+	142  381 # latin capital letter z with caron
+	145 8216 # left single quotation mark
+	146 8217 # right single quotation mark
+	147 8220 # left double quotation mark
+	148 8221 # right double quotation mark
+	149 8226 # bullet
+	150 8211 # en dash
+	151 8212 # em dash
+	152  732 # small tilde
+	153 8482 # trade mark sign
+	154  353 # latin small letter s with caron
+	155 8250 # single right-pointing angle quotation mark
+	156  339 # latin small ligature oe
+	158  382 # latin small letter z with caron
+	159  376 # latin capital letter y with diaeresis
+    } \n] {
+	set line [string trim $line]
+	if {$line eq ""} continue
+	lassign [split [string trim $line]] from to
+	lappend result \\u$from \\u$to
+    }
+    return [subst -nocommands -novariables $result]
+}
+interp alias {} demoronizer {} string map [demoronizer]
 
 namespace eval Html {
     proc attr {T args} {
