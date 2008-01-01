dcaa8f90dbbde8f8a580e3666b9602f3c3b94adb
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-01-01 14:13:06 +0000

    * made blocking persistent

--- trunk/Wub/Block.tcl
+++ trunk/Wub/Block.tcl
@@ -1,17 +1,32 @@
+package require fileutil
+
 package provide Block 2.0
 
 namespace eval Block {
     variable blocked; array set blocked {}
+    variable logdir ""
 
     proc block {ipaddr {reason ""}} {
 	variable blocked
 	set blocked($ipaddr) [list [clock seconds] $reason]
+	::fileutil::appendToFile [file join $logdir blocked] "$ipaddr [list $blocked($ipaddr)]\n"
 	Debug.block {BLOCKING: $ipaddr $reason}
     }
 
     proc blocked? {ipaddr} {
 	variable blocked
 	return [info exists blocked($ipaddr)]
+    }
+
+    proc init {args} {
+	variable {*}$args
+	variable blocked
+	array set blocked [fileutil::cat [file join $logdir blocked]]
+    }
+
+    proc blocklist {} {
+	variable blocked
+	return [array get blocked]
     }
 
     namespace export -clear *
