fbae8a91a871efc1a5f19f547c6430e042cc23f9
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-01-03 13:26:54 +0000

    * made Http redirections use Url's [redir]

--- Utilities/Http.tcl
+++ Utilities/Http.tcl
@@ -594,7 +594,7 @@ namespace eval Http {
 
     # internal redirection generator
     proc genRedirect {title code rsp to {content ""} {ctype "text/html"} args} {
-	set to [Url redir $rsp $to $args]
+	set to [Url redir $rsp $to {*}$args]
 
 	if {$content eq ""} {
 	    dict set rsp content-type "text/html"
@@ -633,12 +633,12 @@ namespace eval Http {
 
     # construct an HTTP Redirect response
     proc Redirect {rsp to {content ""} {ctype "text/html"} args} {
-	return [Http genRedirect Redirect 302 $rsp $to $content $ctype $args]
+	return [Http genRedirect Redirect 302 $rsp $to $content $ctype {*}$args]
     }
 
     # construct a simple HTTP Redirect response with extra query
     proc Redir {rsp to args} {
-	return [Http genRedirect Redirect 302 $rsp $to "" "" $args]
+	return [Http genRedirect Redirect 302 $rsp $to "" "" {*}$args]
     }
 
     # construct an HTTP Redirect response to Referer of request
@@ -647,27 +647,27 @@ namespace eval Http {
 	if {$ref eq ""} {
 	    set ref /
 	}
-	return [Http genRedirect Redirect 302 $rsp $to "" "" $args]
+	return [Http genRedirect Redirect 302 $rsp $to "" "" {*}$args]
     }
 
     # construct an HTTP Found response
     proc Found {rsp to {content ""} {ctype "text/html"} args} {
-	return [Http genRedirect Redirect 302 $rsp $to $content $ctype $args]
+	return [Http genRedirect Redirect 302 $rsp $to $content $ctype {*}$args]
     }
 
     # construct an HTTP Relocated response
     proc Relocated {rsp to {content ""} {ctype "text/html"} args} {
-	return [Http genRedirect Relocated 307 $rsp $to $content $ctype $args]
+	return [Http genRedirect Relocated 307 $rsp $to $content $ctype {*}$args]
     }
     
     # construct an HTTP SeeOther response
     proc SeeOther {rsp to {content ""} {ctype "text/html"} args} {
-	return [Http genRedirect SeeOther 303 $rsp $to $content $ctype $args]
+	return [Http genRedirect SeeOther 303 $rsp $to $content $ctype {*}$args]
     }
 
     # construct an HTTP Moved response
     proc Moved {rsp to {content ""} {ctype "text/html"} args} {
-	return [Http genRedirect Moved 301 $rsp $to $content $ctype $args]
+	return [Http genRedirect Moved 301 $rsp $to $content $ctype {*}$args]
     }
 
     
