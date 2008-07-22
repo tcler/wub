050d53e8db4cbd67ce289e54f04af8ebb90f67c0
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-07-22 00:29:18 +0000

    * armoured pkgIndex against spaces in $dir

--- trunk/Mime/pkgIndex.tcl
+++ trunk/Mime/pkgIndex.tcl
@@ -1,3 +1,3 @@
-package ifneeded Mime 1.0 [list source [file join $dir Mime.tcl]]
-package ifneeded magiclib 1.0 [list source [file join $dir magiclib.tcl]]
-package ifneeded mime-magic 1.0 [list source [file join $dir magic.mime.tcl]]
+package ifneeded Mime 1.0 [list source [file join [list $dir] Mime.tcl]]
+package ifneeded magiclib 1.0 [list source [file join [list $dir] magiclib.tcl]]
+package ifneeded mime-magic 1.0 [list source [file join [list $dir] magic.mime.tcl]]
