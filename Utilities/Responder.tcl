6d809368a37bc95f6059fb7ec15df691659208ae
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-07-29 14:57:29 +0000

    * added new Responder interface - Process and do

--- Utilities/Responder.tcl
+++ Utilities/Responder.tcl
@@ -130,6 +130,75 @@ namespace eval Responder {
 	}
     }
 
+    proc Process {new args} {
+	if {[catch {
+	    inQ put $new	;# add the incoming request to the inQ
+	    
+	    # while idle and there are new requests to procecss
+	    variable working	;# set while we're working
+	    while {!$working && ![catch {inQ get} req eo]} {
+		set working 1
+		set org $req	;# keep a copy in case
+		catch {do $req {*}$args} r eo
+		Debug.responder {Dispatcher: $eo}
+		switch [dict get $eo -code] {
+		    0 -
+		    2 { # ok - return
+			if {![dict exists $r -code]} {
+			    set rsp [Http Ok $r]
+			} else {
+			    set rsp $r
+			}
+		    }
+		    
+		    1 { # error
+			set rsp [Http ServerError $req $r $eo]
+		    }
+		    
+		    3 { # break
+			set working 0
+			break
+		    }
+		    
+		    4 { # continue
+			set working 0
+			continue
+		    }
+		}
+
+		if {[catch {
+		    post $rsp
+		} r eo]} { ;# postprocess response
+		    Debug.responder {POST ERROR: $rsp ($eo)} 1
+		    set rsp [Http ServerError $rsp $r $eo]
+		} else {
+		    set rsp $r
+		}
+		
+		Debug.responder {RESPONSE: $rsp} 6
+		if {![dict exists $rsp -suspend]
+		    && [catch {
+			Send $rsp 		;# send response
+		    } r eo]} {
+		    # failed to send!
+		    Debug.responder {SEND ERROR: $r ($eo)} 1
+		    if {[catch {
+			# we've completely failed to send
+			Send [Http ServerError $org $r $eo]
+		    } r eo]} {
+			Debug.responder {DOUBLE SEND ERROR: $r ($eo)} 1
+			# give up
+		    }
+		} else {
+		    Debug.responder {Sent}
+		}
+		set working 0	;# go idle
+	    }
+	} err eo]} {
+	    Debug.responder {Incoming: $err $eo}
+	}
+    }
+
     variable working 0	;# set while we're working
     ::struct::queue inQ	;# create a queue of pending work
 
