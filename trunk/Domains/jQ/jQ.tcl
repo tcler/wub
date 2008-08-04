25f4c5304a4cc847e74dcad0fc630e11fdc31b17
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-08-04 11:04:12 +0000

    * removed galleria defaults

--- trunk/Domains/jQ/jQ.tcl
+++ trunk/Domains/jQ/jQ.tcl
@@ -55,46 +55,6 @@ namespace eval jQ {
 	galleria {
 	    history true
 	    clickNext true
-	    insert \"#main_image\"
-	    onImage {
-		function(image,caption,thumb) { // let's add some image effects for demonstration purposes
-		    
-		    // fade in the image & caption
-		    image.css('display','none').fadeIn(100);
-		    caption.css('display','none').fadeIn(100);
-		    
-		    // fetch the thumbnail container
-		    var _li = thumb.parents('li');
-		    
-		    // fade out inactive thumbnail
-		    _li.siblings().children('img.selected').fadeTo(50,0.3);
-		    
-		    // fade in active thumbnail
-		    thumb.fadeTo('fast',1).addClass('selected');
-		    
-		    // add a title for the clickable image
-		    image.attr('title','Next image >>');
-		}
-	    }
-	    onThumb {
-		function(thumb) { // thumbnail effects goes here
-				
-		    // fetch the thumbnail container
-		    var _li = thumb.parents('li');
-		    
-		    // if thumbnail is active, fade all the way.
-		    var _fadeTo = _li.is('.active') ? '1' : '0.3';
-		    
-		    // fade in the thumbnail when finnished loading
-		    thumb.css({display:'none',opacity:_fadeTo}).fadeIn(1500);
-		    
-		    // hover effects
-		    thumb.hover(
-				function() { thumb.fadeTo('fast',1); },
-				function() { _li.not('.active').children('img').fadeTo('fast',0.3); } // don't fade out if the parent is active
-				)
-		}
-	    }
 	}
 
 	map {
