b38474f5834d056bc294f2733cc0e6bc60bae8e4
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-08-28 07:01:20 +0000

    * modified Debug to be a bit cleaner (thanks LarsH) * minor cleanup Http

--- Utilities/Debug.tcl
+++ Utilities/Debug.tcl
@@ -28,14 +28,14 @@ namespace eval Debug {
 	    set fd $fds($tag)
 
 	    set code [catch {
-		uplevel 1 ::subst -nobackslashes [list $message]
+		uplevel 1 [list ::subst -nobackslashes [list $message]]
 	    } result eo]
 	    if {$code} {
 		set x [info level -1]
 		puts -nonewline $fd @@[string map {\n \\n \r \\r} "(DebugError from $tag [if {[string length $x] < 1000} {set x} else {set x "[string range $x 0 200]...[string range $x end-200 end]"}] ($eo)):"]
 	    } else {
 		if {[string length $result] > 4096} {
-		    set result "[string range $result 0 4096]...(truncated) ... [string range $result end-4096 end]"
+		    set result "[string range $result 0 2048]...(truncated) ... [string range $result end-2048 end]"
 		}
 		puts $fd "$tag @@[string map {\n \\n} $result]"
 	    }
