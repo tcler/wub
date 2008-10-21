0fd6813022db5b89b62f12f278ebb1d4a0bf98ea
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2008-10-21 02:34:59 +0000

    * send a Cache-control "must-revalidate" with every NotModified ... see if this helps crazy cache heuristics.

--- Utilities/Http.tcl
+++ Utilities/Http.tcl
@@ -651,6 +651,9 @@ namespace eval Http {
 	# than Date, Expires, Cache-Control, Vary, Etag, Content-Location
 	set result [dict filter $rsp key -*]
 
+	# tell the other end that this isn't the last word.
+	dict set rsp cache-control "must-revalidate"
+
 	variable rq_headers
 	set result [dict merge $result [Dict subset $rsp $rq_headers]]
 
