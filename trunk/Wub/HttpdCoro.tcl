a88dd99b1dd6059193b72026f6329a4a06d42108
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-01-07 00:50:31 +0000

    * attempt to protect against dup consumers

--- trunk/Wub/HttpdCoro.tcl
+++ trunk/Wub/HttpdCoro.tcl
@@ -956,8 +956,8 @@ namespace eval Httpd {
 	if {[info commands $cr] ne ""} {
 	    # the consumer seems to be lingering - we have to tell it to die
 	    set cn ::Httpd::CO_DEAD_[uniq]
-	    rename $cr $cn	;# move it out of the way first
 	    Debug.log {consumer $cr not dead yet, rename to $cn to kill it.}
+	    rename $cr $cn	;# move it out of the way first
 	    after 1 [list $cn [list EOF "socket's gone"]]	;# ensure the old consumer's dead
 	}
 	variable consumer; coroutine $cr ::apply [list args $consumer ::Httpd] reader $R
