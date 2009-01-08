c658653d2da2218d6d94582d8726e9b6f6908890
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-01-08 00:44:21 +0000

    * added debug on sent header

--- Wub/HttpdCoro.tcl
+++ Wub/HttpdCoro.tcl
@@ -260,7 +260,7 @@ namespace eval Httpd {
 
 	    # send headers with terminating nl
 	    chan puts -nonewline $socket "$head\r\n"
-	    Debug.HttpdCoro {SENT HEADER: $socket [string length $head] bytes} 4
+	    Debug.HttpdCoro {SENT HEADER: $socket '[lindex [split $head \r] 0]' [string length $head] bytes} 4
 
 	    # send the content/entity (if any)
 	    # note: we must *not* send a trailing newline, as this
