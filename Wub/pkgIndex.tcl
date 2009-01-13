d13ab9106c6d982d2860f9928034a2a0790d6649
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-01-13 00:51:14 +0000

    * Upgraded to Wub 4.0

--- Wub/pkgIndex.tcl
+++ Wub/pkgIndex.tcl
@@ -1,15 +1,6 @@
 package ifneeded Listener 2.0 [list source [file join $dir Listener.tcl]]
-
-package ifneeded Httpd 3.0 [list source [file join $dir Httpd.tcl]]
-package ifneeded HttpdThread 1.0 [list source [file join $dir HttpdThread.tcl]]
-package ifneeded HttpdSingle 1.0 [list source [file join $dir HttpdSingle.tcl]]
-package ifneeded HttpdCoro 1.0 [list source [file join $dir HttpdCoro.tcl]]
-
+package ifneeded Httpd 4.0 [list source [file join $dir Httpd.tcl]]
 package ifneeded Cache 2.0 [list source [file join $dir Cache.tcl]]
 package ifneeded Varnish 1.0 [list source [file join $dir Varnish.tcl]]
-package ifneeded Access 1.0 [list source [file join $dir Access.tcl]]
-package ifneeded Activity 2.0 [list source [file join $dir Activity.tcl]]
 package ifneeded Block 2.0 [list source [file join $dir Block.tcl]]
-
-package ifneeded Backend 1.0 [list source [file join $dir Backend.tcl]]
 package ifneeded Site 1.0 [list source [file join $dir Site.tcl]]
