04117a496a34b0cab7ddbc719e975fcea7161e94
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-01-14 05:12:08 +0000

    * made truly minimal

--- trunk/Wub/Minimal.tcl
+++ trunk/Wub/Minimal.tcl
@@ -3,40 +3,11 @@
 # Single Threaded Minimal File Server Site
 lappend auto_path [pwd]	;# path to the Site.tcl file
 namespace eval Site {
-    variable home [file dirname [info script]]
+    variable home [file dirname [file normalize [info script]]]
+    variable docroot [file join [file dirname $home] docs]
+    variable httpd [list log stderr]
 }
-
 package require Site
-
-# This is a File domain which serves simple files
-# from the eponymous directory.  Each file served has an expiry
-# date as indicated, which allows caching.
-
-# File domain is the most minimal, but Mason is better
-# File docroot -root $::Site::docroot
-Mason create wub -url / -root $docroot -auth .before -wrapper .after
-
-# [Responder Incoming] provides a safe wrapper and switch-like
-# dispatcher for incoming requests.
-proc Responder::do {req} {
-    switch -glob -- [dict get $req -path] {
-	/ -
-	/* {
-	    # redirect / to /wub
-	    return [docroot do $req]
-	}
-    }
-}
-
-#### Incoming - handle of incoming request
-#
-# This proc is called with a fully parsed incoming request.
-# It uses Responder to wrap a tcl's [switch] dispatch command,
-# and wrap errors in an appropriate HTML response.
-
-proc Incoming {req} {
-    return [Responder Process $req]
-}
 
 # Start Site Server(s)
 Site start
