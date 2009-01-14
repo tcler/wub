fbb309745edd35da6cead7fc809572751a3e2a0f
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-01-14 05:16:04 +0000

    * removed Widget package

--- Domains/pkgIndex.tcl
+++ Domains/pkgIndex.tcl
@@ -26,7 +26,5 @@ package ifneeded Tie  1.0 [list source [file join $dir Tie.tcl]]
 package ifneeded Rest  1.0 [list source [file join $dir Rest.tcl]]
 package ifneeded Coco  1.0 [list source [file join $dir Coco.tcl]]
 
-package ifneeded Widget 1.0 [list source [file join $dir Widget.tcl]]
-
 package ifneeded Sinorca 1.0 [list source [file join $dir Sinorca Sinorca.tcl]]
 package ifneeded Simplicio 1.0 [list source [file join $dir Simplicio.tcl]]
