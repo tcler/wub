e53732a4914f971b3dfba7fdcdca5da3e26dd45c
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-02-13 00:00:53 +0000

    * pared down Application

--- trunk/Wub/Application.tcl
+++ trunk/Wub/Application.tcl
@@ -1,12 +1,9 @@
 #! /usr/bin/env tclsh
-
-# Single Threaded Simplistic Site
-lappend auto_path [pwd]	;# path to the Site.tcl file
-namespace eval Site {
-    variable home [file dirname [info script]]
-}
-
+lappend auto_path [pwd]
 package require Site
 
+# Initialize Site
+Site init home [file normalize [file dirname [info script]]] ini site.ini
+
 # Start Site Server(s)
-Site start
+Site start 
