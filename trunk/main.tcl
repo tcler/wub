e6015b453abd5d43da8f16f711ee7eca69b2dc07
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-02-15 12:29:56 +0000

    *** empty log message ***

--- trunk/main.tcl
+++ trunk/main.tcl
@@ -6,10 +6,8 @@ starkit::startup
 package require Site
 
 if {![dict exists $argv application]} {
-    set userdir [pwd]	;# this is the dir from which the user ran the kit
-
     # Initialize Site
-    Site init ini site.ini userdir $userdir
+    Site init ini site.ini home [pwd]	;# this is the dir from which the user ran the kit
 
     # Start Site Server(s)
     Site start
