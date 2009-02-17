6dced6be35e006f41b1afc4cc411e2fba0df5b8d
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-02-17 23:27:40 +0000

    * removed all caching of ext<->mime bijection * incidentally removed much of the configurability of the package - now mapping has to be established at startup.

--- trunk/Mime/Mime.tcl
+++ trunk/Mime/Mime.tcl
@@ -1,32 +1,24 @@
-package provide Mime 1.0
+# Mime - calculate the mime types of file
 
-package require cmdline
-package require tie
 package require WubUtils
 
-# E2M - simple minded file extension->mime-type map
-# persisted using ::tie
-namespace eval E2M {
-    variable home ""
+package provide Mime 1.0
 
+namespace eval Mime {
+    variable mimecache
+    variable file_attributes 1	;# should we seek file attributes to tell us?
+    variable home [file dirname [info script]]
     variable e2m
     array set e2m {}
-    variable initialized 0
 
     variable default text/plain	;# default mime type
-    variable prime ~/mime.types	;# file to prime map
+    variable prime mime.types	;# file to prime map
 
-    variable tie
+    # simple minded file extension<->mime-type map
 
-    # Tie configuration variables, each of which can be
-    # initialized by calling init with their names as -flags
-    variable dstype file
-    variable dsname ext2mime.tie
-    variable dsargs {}
-
-    # Add --
+    # add --
     #
-    # 	Add a MIME type mapping
+    # 	add a MIME type mapping
     #
     # Arguments:
     #	suffix	A file suffix
@@ -35,85 +27,17 @@ namespace eval E2M {
     # Results:
     #       None
 
-    proc Add {ext type} {
+    proc add {ext type} {
 	variable e2m
 	set ext [string tolower [string trimleft $ext .]]
 	set e2m(.$ext) $type
 	lappend e2m($type) .$ext
     }
 
-    proc MimeOf {ext} {
-	variable e2m
-
-	if {!$::E2M::initialized} {
-	    Init	;# lazy init, to allow user customisation
-	}
-
-	set ext ".[string trim [string tolower $ext] .]"
-	if {[info exist e2m($ext)]} {
-	    return $e2m($ext)
-	} else {
-	    return $e2m()
-	}
-    }
-
-    # init the thing
-    proc Init {args} {
-	#puts stderr "Mime Init"
-	variable dstype
-	variable dsname
-	variable dsargs
-	variable prime
-
-	foreach {name val} [::cmdline::getKnownOptions args [subst {
-	    {dsname.arg [list $dsname]
-		"tie data storage name"
-	    }
-	    {dstype.arg [list $dstype]
-		"tie data storage type"
-	    }
-	    {dsargs.arg [list $dsargs]
-		"tie data storage args"
-	    }
-	    {prime.arg [list $prime]
-		"mime type mapping"
-	    }
-	}]] {
-	    set $name $val
-	}
-
-	variable home
-	set prime [string map [list ~ $home] $prime]
-
-	variable tie
-	variable e2m
-
-	set reread 0
-	if {[file exists $dsname] && [file exists $prime]} {
-	    set reread [file newer $prime $dsname]
-	} elseif {![file exists $dsname]} {
-	    set reread 1
-	}
-
-	set tie [::tie::tie e2m -open $dstype $dsname {*}$dsargs]
-
-	if {$reread} {
-	    variable default
-	    set e2m() $default
-
-	    catch {Read $prime}
-	}
-
-	::tie::untie e2m $tie
-	set tie [::tie::tie e2m -save $dstype $dsname {*}$dsargs]
-
-	variable initialized 1	;# so we know we've been initialized
-    }
-
-    proc Read {file} {
+    proc read {file} {
 	package require fileutil
 	variable e2m
-	puts stderr "Mime Read $file"
+	#puts stderr "Mime read $file"
 
 	::fileutil::foreachLine line $file {
 	    set line [string trim $line]
@@ -128,24 +52,47 @@ namespace eval E2M {
 	    } else {
 		if {[string match {*/*} [lindex $line 0]]} {
 		    foreach ext [lrange $line 1 end] {
-			Add $ext [lindex $line 0]
+			add $ext [lindex $line 0]
 		    }
 		} else {
-		    Add [lindex $line 0] [lindex $line 1]
+		    add [lindex $line 0] [lindex $line 1]
 		}
 	    }
 	}
     }
 
-    namespace export Add MimeOf Init Read
-}
+    proc MimeOf {ext} {
+	variable e2m
 
-namespace eval Mime {
-    namespace import ::E2M::*
+	init	;# lazy init, to allow user customisation
 
-    variable mimecache
-    variable file_attributes 1
-    variable debug 20
+	set ext ".[string trim [string tolower $ext] .]"
+	if {[info exist e2m($ext)]} {
+	    return $e2m($ext)
+	} else {
+	    return $e2m()
+	}
+    }
+
+    # init the thing
+    proc init {args} {
+	#puts stderr "Mime init"
+	variable home
+	variable tie
+	variable e2m
+	variable prime
+
+	if {[file pathtype $prime] eq "relative"} {
+	    set prime [file join $home $prime]
+	}
+
+	# set the default mimetype
+	variable default
+	set e2m() $default
+
+	catch {read $prime}
+	proc init {args} {}	;# can only be initialized once.
+    }
 
     array set mimecache {}
     proc type {file} {
@@ -220,12 +167,7 @@ namespace eval Mime {
 	    }
 	}
 	
-	# then fall back to file extension to mime type mapping
-	if {!$::E2M::initialized} {
-	    ::E2M::Init	;# lazy init, to allow user customisation
-	}
-
-	set mimecache($file) [E2M::MimeOf [file extension $file]]
+	set mimecache($file) [MimeOf [file extension $file]]
 	# (note: we don't store e2m mime types in file attributes) Why?
 
 	Debug.mime {MIME ext: $mimecache($file)}
@@ -236,5 +178,3 @@ namespace eval Mime {
     namespace export -clear *
     namespace ensemble create -subcommands {}
 }
-
-set E2M::home [file dirname [info script]]
