9d2f94552fc79a9f3c7b1cdc38ce404d681eadab
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-03-19 09:18:51 +0000

    * added user-agent classification to access log

--- trunk/Utilities/Http.tcl
+++ trunk/Utilities/Http.tcl
@@ -174,7 +174,7 @@ namespace eval Http {
 
 	# referer, useragent, cookie, if any
 	lappend line \"[dict get? $r referer]\"
-	lappend line \"[dict get? $r user-agent]\"
+	lappend line \"[dict get? $r user-agent] ([dict get? $r -ua_class])\"
 	lappend line \"[dict get? $r cookie]\"
 
 	return [join $line]
