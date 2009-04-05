252292cc31bad06ca7a25bc8feb975e659b90921
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-04-05 23:38:29 +0000

    * moved to [urlsuffix] processing

--- trunk/Domains/File.tcl
+++ trunk/Domains/File.tcl
@@ -9,7 +9,7 @@ package provide File 2.0
 package require Report
 package require jQ
 
-set API(File) {
+set API(Domains/File) {
     {provides a traditional Web view for filesystem hierarchies.}
     root {filesystem root directory of File domain}
     indexfile {name of the file which stands for a directory, such as index.html}
@@ -54,53 +54,45 @@ class create File {
 	return $req
     }
 
-    method do {req} {
+    method do {r} {
 
-	if {[dict exists $req -suffix]} {
-	    # caller has munged path already
-	    set suffix [dict get $req -suffix]
-	} else {
-	    # assume we've been parsed by package Url
-	    # remove the specified mount from path, giving suffix
-	    set suffix [Url pstrip $mount [dict get $req -path]]
-	    if {($suffix ne "/") && [string match "/*" $suffix]} {
-		# path isn't inside our domain suffix - error
-		return [Http NotFound $req]
-	    }
-	    dict set req -suffix $suffix
+	# calculate the suffix of the URL relative to $mount
+	lassign [Url urlsuffix $r $mount] result r suffix
+	if {!$result} {
+	    return $r	;# the URL isn't in our domain
 	}
 
 	set ext [file extension $suffix]
 	set path [file join $root [string trimleft $suffix /]]
 
-	Debug.file {file: root:$root mount:$mount suffix:$suffix path:$path -path:[dict get $req -path]}
+	Debug.file {file: root:$root mount:$mount suffix:$suffix ext: $ext path:$path -path:[dict get $r -path]}
 
 	if {($ext ne "")
 	    && ([file tail $suffix] eq $ext)
-	    && ![dict exists $req -extonly]
+	    && ![dict exists $r -extonly]
 	} {
 	    # this is a file name like '.tml'
-	    return [Http NotFound $req "<p>File '$suffix' has illegal name.</p>"]
+	    return [Http NotFound $r "<p>File '$suffix' has illegal name.</p>"]
 	}
 	
 	if {![file exists $path]} {
 	    # if the file doesn't exist, say so.
-	    return [Http NotFound $req "<p>File '$suffix' doesn't exist</p>"]
+	    return [Http NotFound $r "<p>File '$suffix' doesn't exist</p>"]
 	}
 
 	# handle conditional request
-	if {[dict exists $req if-modified-since]
-	    && (![dict exists $req -dynamic] || ![dict get $req -dynamic])
+	if {[dict exists $r if-modified-since]
+	    && (![dict exists $r -dynamic] || ![dict get $r -dynamic])
 	} {
-	    set since [Http DateInSeconds [dict get $req if-modified-since]]
+	    set since [Http DateInSeconds [dict get $r if-modified-since]]
 	    if {[file mtime $path] <= $since} {
-		Debug.file {NotModified: $path - [Http Date [file mtime $path]] < [dict get $req if-modified-since]}
+		Debug.file {NotModified: $path - [Http Date [file mtime $path]] < [dict get $r if-modified-since]}
 		Debug.file {if-modified-since: not modified}
-		return [Http NotModified $req]
+		return [Http NotModified $r]
 	    }
 	}
 	
-	Debug.file {FILE DISPATCH '$path' $req}
+	Debug.file {FILE DISPATCH '$path' $r}
 	Debug.file {Found file '$path' of type [file type $path]}
 	set count 20
 	while {[incr count -1]} {
@@ -115,17 +107,17 @@ class create File {
 
 		file {
 		    # allow client caching
-		    set r [Http Cache $req $expires]
+		    set r [Http Cache $r $expires]
 		    return [Http CacheableFile $r $path]
 		}
 		
 		directory {
 		    # if a directory reference doesn't end in /, redirect.
-		    Debug.file {redirecting path:$path, suffix:$suffix, -path:[dict get $req -path]}
-		    set rpath [dict get $req -path]
+		    Debug.file {redirecting path:$path, suffix:$suffix, -path:[dict get $r -path]}
+		    set rpath [dict get $r -path]
 		    if {$redirdir && ([string index $rpath end] ne "/")} {
-			dict set req -path "$rpath/"
-			return [Http Redirect $req [Url uri $req]]
+			dict set r -path "$rpath/"
+			return [Http Redirect $r [Url uri $r]]
 		    } else {
 			# TODO do something to return the whole dir in one hit
 		    }
@@ -134,27 +126,27 @@ class create File {
 		    if {$indexfile ne ""} {
 			set indices [glob -nocomplain -tails -directory $path $indexfile]
 			if {[llength $indices]} {
-			    dict set req -path [file join [dict get $req -path] [lindex $indices 0]]
-			    return [Http Redirect $req [Url uri $req]]
+			    dict set r -path [file join [dict get $r -path] [lindex $indices 0]]
+			    return [Http Redirect $r [Url uri $r]]
 			}
 		    }
 		    if {$nodir} {
 			return [Http NotFound "No Such Directory"]
 		    } else {
 			# no index file - generate a directory listing
-			set req [my dir $req $path]
-			return [Http CacheableContent [Http Cache $req $expires] [clock seconds]]
+			set r [my dir $r $path]
+			return [Http CacheableContent [Http Cache $r $expires] [clock seconds]]
 		    }
 		}
 		
 		default {
-		    set req [Http Cache $req $expires]
-		    return [Http NotFound $req "<p>File '$suffix' is of illegal type [file type $path]</p>"]
+		    set r [Http Cache $r $expires]
+		    return [Http NotFound $r "<p>File '$suffix' is of illegal type [file type $path]</p>"]
 		}
 	    }
 	}
 
-	return [Http NotFound $req "<p>File '$suffix' doesn't resolve to a file.</p>"]
+	return [Http NotFound $r "<p>File '$suffix' doesn't resolve to a file.</p>"]
     }
 
     variable root indexfile mount hide redirdir expires dateformat dirparams nodir
