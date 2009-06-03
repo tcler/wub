7021d563f1a412e210a4c55bc1a5654c855895b7
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-06-03 08:44:42 +0000

    * made Resume ignore errors (perhaps unwisely)

--- trunk/Wub/Httpd.tcl
+++ trunk/Wub/Httpd.tcl
@@ -1456,7 +1456,7 @@ namespace eval Httpd {
     # resume this request
     proc Resume {r {cache 1}} {
         # ask socket coro to send the response for us
-	{*}[dict get $r -send] [list SEND $r]
+	catch {{*}[dict get $r -send] [list SEND $r]}
     }
 
     # every script
