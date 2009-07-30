8bdcf538c05a5c24fca1eff44ae0b41902aca6d6
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-07-30 15:01:00 +0000

    * removed some debug narrative

--- Wub/Httpd.tcl
+++ Wub/Httpd.tcl
@@ -1505,7 +1505,6 @@ namespace eval Httpd {
 	#Debug on HttpdLow 10
 	if {[info commands ::wub] eq {}} {
 	    package require Mason
-	    puts stderr "DOCS: $::Site::docroot"
 	    Mason create ::wub -url / -root $::Site::docroot -auth .before -wrapper .after
 	}
 
