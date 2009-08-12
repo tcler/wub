7a0b24cd99ad099157fe164d348012c2f59bef71
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-08-12 02:30:10 +0000

    * added OO package for useful tclOO stuff

---
+++ trunk/Utilities/OO.tcl
@@ -0,0 +1,62 @@
+# OO.tcl - helpers for tclOO
+package require TclOO
+namespace import oo::*
+
+package provide OO 1.0
+
+proc ::oo::Helpers::classvar {name args} {
+    set ns [info object namespace [uplevel 1 {self class}]]
+    foreach v [list $name {*}$args] {
+	uplevel 1 [list namespace upvar $ns $v $v]
+    }
+}
+
+proc ::oo::define::classmethod {name {args {}} {body {}}} {
+    set class [lindex [info level -1] 1]
+    set classmy [info object namespace $class]::my
+    if {[llength [info level 0]] == 4} {
+        uplevel 1 [list self method $name $args $body]
+    }
+    uplevel 1 forward $name $classmy $name
+}
+
+proc oo::define::Variable args {
+    set currentclass [lindex [info level 1] 1]
+    set existing [uplevel 1 [list info class variables $currentclass]]
+    switch [lindex $args 0] {
+        -append {
+            set vars $existing
+            lappend vars {*}[lrange $args 1 end]
+        }
+        -prepend {
+            set vars [lrange $args 1 end]
+            lappend vars {*}$existing
+        }
+        -remove {
+            set vars $existing
+            foreach var [lrange $args 1 end] {
+                set idx [lsearch -exact $vars $var]
+                if {$idx >= 0} {
+                    set vars [lreplace $vars $idx $idx]
+                }
+            }
+        }
+        -set - default {
+            set vars [lrange $args 1 end]
+        }
+    }
+    uplevel 1 [list variables {*}$vars]
+    return
+}
+
+# lets you do this:
+# oo::class create foo {
+#     Variable x y
+#     Variable -append p d q
+#     method bar args {
+#         lassign $args x y p d q
+#     }
+#     method boo {} {
+#         return $x,$y|$p,$d,$q
+#     }
+# }
