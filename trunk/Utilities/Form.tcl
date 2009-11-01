6cdaee503d8af5518e0d72608267814c0bc383a5
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-11-01 02:04:51 +0000

    * added tabular form form

--- trunk/Utilities/Form.tcl
+++ trunk/Utilities/Form.tcl
@@ -49,21 +49,22 @@ package require textutil
 package require Dict
 package require Html
 
-#package require Debug
-Debug off form 10
+package require Debug
 
 package provide Form 2.0
 
 namespace eval Form {
     variable Fdefaults [dict create {*}{
-	textarea {-compact 0}
-	form {method post}
+	textarea {compact 0}
+	form {method post tabular 0}
 	fieldset {vertical 0}
 	submit {alt Submit}
 	reset {alt Reset}
 	option {}
     }]
     variable tabindex 0	;# taborder for fields
+    variable tabular 0	;# tabular form elements
+    variable uniqID 0	;# unique ID for fields
 
     # default - set attribute defaults for a given tag
     proc default {type args} {
@@ -94,8 +95,13 @@ namespace eval Form {
 	variable fieldsetA
 	variable Fdefaults
 	set config [dict merge [dict get? $Fdefaults fieldset] [lrange $args 0 end-1]]
-	if {$name ne "" && ![dict exists $config id]} {
-	    dict set config id $name
+	if {![dict exists $config id]} {
+	    if {$name ne ""} {
+		dict set config id $name
+	    } else {
+		variable uniqID
+		dict set config id F[incr uniqID]
+	    }
 	}
 	set content "<[attr fieldset [Dict subset $config $fieldsetA]]>\n"
 	if {[dict exists $config legend]} {
@@ -107,9 +113,15 @@ namespace eval Form {
     proc formS {name args} {
 	variable formA
 	variable Fdefaults
+	variable tabindex 0
 	set config [dict merge [dict get? $Fdefaults form] [lrange $args 0 end-1]]
-	if {$name ne "" && ![dict exists $config id]} {
-	    dict set config id $name
+	if {![dict exists $config id]} {
+	    if {$name ne ""} {
+		dict set config id $name
+	    } else {
+		variable uniqID
+		dict set config id F[incr uniqID]
+	    }
 	}
 	set content "<[attr form [Dict subset $config $formA]]>\n"
 	if {[dict exists $config legend]} {
@@ -120,76 +132,99 @@ namespace eval Form {
 
     # <form> and <fieldset>
     foreach {type} {form fieldset} {
-	if {0 && $type eq "form"} {
-	    set ti "variable tabindex 0"
+	if {$type eq "form"} {
+	    set ti {
+		variable tabindex 0
+	    }
 	} else {
 	    set ti ""
 	}
-	eval [string map [list @TI $ti @T $type] {
-	    proc <@T> {args} {
-		variable @TA
+	eval [string map [list %TI% $ti %T% $type] {
+	    proc <%T%> {args} {
+		variable %T%A
 		variable Fdefaults
+
 		set body [lindex $args end]
 		set args [lrange $args 0 end-1]
 
+		# get form name from args, if present
 		set name ""
 		if {[llength $args]%2} {
 		    set args [lassign $args name]
-		} 
-
-		set config [dict merge [dict get? $Fdefaults @T] $args]
-		if {$name ne "" && ![dict exists $config id]} {
-		    dict set config id $name
 		}
 
+		set config [dict merge [dict get? $Fdefaults %T%] $args]
+		%TI%
+
+		variable tabular
+		set otab $tabular
+		if {[dict exists $config tabular] && [dict get $config tabular]} {
+		    set tabular 1
+		} else {
+		    set tabular 0
+		}
+
+		# ensure an id
+		if {![dict exists $config id]} {
+		    if {$name ne ""} {
+			dict set config id $name
+		    } else {
+			variable uniqID
+			dict set config id F[incr uniqID]
+		    }
+		}
+
+		# evaluate form content
+		set body [uplevel 1 [list subst $body]]
+		set body [string trim $body " \t\n\r"]
+
+		set content ""
 		if {[dict exists $config legend]} {
 		    append content [<legend> [dict get $config legend]] \n
 		}
-
-		@TI
-		append content [uplevel 1 [list subst $body]]
-		set content [string trim $content " \t\n\r"]
-
-		if {[dict exists $config vertical]
-		    && [dict get $config vertical]
-		} {
-		    set content [string map {\n <br>\n} [string trim $content \n]]
+		if {$tabular} {
+		    set body [<tr> [string map {\n </tr>\n<tr>} $body]]
+		    append content \n [<table> $body] \n
+		} elseif {[dict exists $config vertical]
+			   && [dict get $config vertical]
+		      } {
+		    append content $body
+		    set content [string map {\n <br>\n} $content]
+		} else {
+		    append content $body
 		}
+		set tabular $otab	;# restore old tabular value
 
-		return "<[attr @T [Dict subset $config $@TA]]>$content</@T>"
+		return "<[attr %T% [Dict subset $config $%T%A]]>$content</%T%>"
 	    }
 	}]
     }
     
     foreach {type} {legend} {
-	eval [string map [list @T $type] {
-	    proc <@T> {args} {
-		variable @TA
+	eval [string map [list %T% $type] {
+	    proc <%T%> {args} {
+		variable %T%A
 		variable Fdefaults
-		set config [dict merge [dict get? $Fdefaults @T] [lrange $args 0 end-1]]
-		return "<[attr @T [Dict subset $config $@TA]]>[uplevel 1 [list subst [lindex $args end]]]</@T>"
+		set config [dict merge [dict get? $Fdefaults %T%] [lrange $args 0 end-1]]
+		return "<[attr %T% [Dict subset $config $%T%A]]>[uplevel 1 [list subst [lindex $args end]]]</%T%>"
 	    }
 	}]
     }
-    
+
     proc <select> {name args} {
 	variable selectA
 	variable Fdefaults
+
 	set content [lindex $args end]
 	set args [lrange $args 0 end-1]
-	Debug.form {<select> $name ($args) content:'$content'}
 	set config [dict merge [dict get? $Fdefaults select] $args [list name $name]]
 
 	if {![dict exists $config id]} {
-	    if {[dict exists $config label]} {
-		dict set config id $name
-		set id $name
-	    }
-	} else {
-	    set id [dict get $config id]
+	    dict set config id $name
 	}
+	set id [dict get $config id]
 
-	if {0 && ![dict exists $config tabindex]} {
+	if {![dict exists $config tabindex]} {
 	    variable tabindex
 	    dict set config tabindex [incr tabindex]
 	}
@@ -204,9 +239,15 @@ namespace eval Form {
 	    set title {}
 	}
 
-	if {[dict exists $config label]} {
-	    set label [dict get $config label]
-	    return [<label> for $id "$label $result"]
+	set label [dict get? $config label]
+	variable tabular
+	if {$tabular} {
+	    if {$label eq ""} {
+		set label $name
+	    }
+	    return "[<td> class label [<label> for $id $label]] [<td> class field $result]"
+	} elseif {$label ne ""} {
+	    return "[<label> for $id $label] $result"
 	} elseif {[dict exists $config legend]} {
 	    set legend [dict get $config legend]
 
@@ -227,16 +268,16 @@ namespace eval Form {
 	    return $result
 	}
     }
-    
+
     foreach {type} {option optgroup} {
-	eval [string map [list @T $type] {
-	    proc <@T> {args} {
-		variable @TA
+	eval [string map [list %T% $type] {
+	    proc <%T%> {args} {
+		variable %T%A
 		variable Fdefaults
 
 		set content [lindex $args end]
 		set args [lrange $args 0 end-1]
-		set config [dict merge [dict get? $Fdefaults @T] $args]
+		set config [dict merge [dict get? $Fdefaults %T%] $args]
 		if {$content eq ""} {
 		    set content [dict get $config value]
 		} else {
@@ -249,7 +290,7 @@ namespace eval Form {
 		    }
 		}
 
-		return "<[attr @T [Dict subset $config $@TA]]>$content</@T>"
+		return "<[attr %T% [Dict subset $config $%T%A]]>$content</%T%>"
 	    }
 	}]
     }
@@ -265,31 +306,25 @@ namespace eval Form {
 	}
 	set config [dict merge [dict get? $Fdefaults textarea] $args [list name $name]]
 
-	if {0 && ![dict exists $config tabindex]} {
+	if {![dict exists $config tabindex]} {
 	    variable tabindex
 	    dict set config tabindex [incr tabindex]
 	}
 
-	if {$name ne "" && ![dict exists $config id]} {
+	# ensure an id
+	if {![dict exists $config id]} {
 	    dict set config id $name
 	}
+	set id [dict get $config id]
 
-	if {![dict exists $config id]} {
-	    if {[dict exists $config label]} {
-		dict set config id $name
-		set id $name
+	if {[dict exists $config compact]} {
+	    if {[dict get $config compact]} {
+		# remove initial spaces from Form
+		set content [::textutil::undent [::textutil::untabify $content]]
 	    }
-	} else {
-	    set id [dict get $config id]
+	    dict unset config compact
 	}
-
-	if {[dict exists $config -compact]
-	    && [dict get $config -compact]
-	} {
-	    # remove initial spaces from Form
-	    set content [::textutil::untabify2 [::textutil::undent [::textutil::tabify2 $content]]]
-	}
-
+	
 	set title {}
 	if {[dict exists $config title]
 	    && ([dict exists $config label]
@@ -300,9 +335,15 @@ namespace eval Form {
 	}
 	set result "<[attr textarea [Dict subset $config $textareaA]]>$content</textarea>"
 
-	if {[dict exists $config label]} {
-	    set label [dict get $config label]
-	    return [<label> for $id "$label $result"]
+	set label [dict get? $config label]
+	variable tabular
+	if {$tabular} {
+	    if {$label eq ""} {
+		set label $name
+	    }
+	    return "[<td> class label [<label> for $id $label]] [<td> class field $result]"
+	} elseif {$label ne ""} {
+	    return "[<label> for $id $label] $result"
 	} elseif {[dict exists $config legend]} {
 	    set legend [dict get $config legend]
 
@@ -323,10 +364,11 @@ namespace eval Form {
 	    return $result
 	}
     }
-    
+
+    # <reset> and <submit>
     foreach type {reset submit} {
-	eval [string map [list @T $type] {
-	    proc <@T> {name args} {
+	eval [string map [list %T% $type] {
+	    proc <%T%> {name args} {
 		variable Fdefaults
 		if {[llength $args] % 2} {
 		    set content [lindex $args end]
@@ -334,9 +376,9 @@ namespace eval Form {
 		} else {
 		    set content ""
 		}
-		set config [dict merge [dict get? $Fdefaults @T] [list alt @T] $args [list name $name type @T]]
+		set config [dict merge [dict get? $Fdefaults %T%] [list alt %T%] $args [list name $name type %T%]]
 		
-		if {0 && ![dict exists $config tabindex]} {
+		if {![dict exists $config tabindex]} {
 		    variable tabindex
 		    dict set config tabindex [incr tabindex]
 		}
@@ -345,7 +387,7 @@ namespace eval Form {
 		    if {[dict exists $config content]} {
 			set content [dict get $config content]
 		    } else {
-			set content [string totitle @T]
+			set content [string totitle %T%]
 		    }
 		} else {
 		    set content [uplevel 1 subst [list $content]]
@@ -361,7 +403,7 @@ namespace eval Form {
 	variable Fdefaults
 	set config [dict merge [dict get? $Fdefaults button] [lrange $args 0 end-1] [list name $name]]
 
-	if {0 && ![dict exists $config tabindex]} {
+	if {![dict exists $config tabindex]} {
 	    variable tabindex
 	    dict set config tabindex [incr tabindex]
 	}
@@ -377,8 +419,8 @@ namespace eval Form {
 	file file value
 	image image src
     } {
-	eval [string map [list @T $itype @A $attrs @F $field] {
-	    proc <@T> {name args} {
+	eval [string map [list %T% $itype %A% $attrs %F% $field] {
+	    proc <%T%> {name args} {
 		if {[llength $args] % 2} {
 		    set value [lindex $args end]
 		    set args [lrange $args 0 end-1]
@@ -386,23 +428,19 @@ namespace eval Form {
 		    set value ""
 		}
 		
-		variable @AA
+		variable %A%A
 		variable Fdefaults
-		set config [dict merge [dict get? $Fdefaults @T] $args [list name $name type @T @F [armour [uplevel 1 [list subst $value]]]]]
+		set config [dict merge [dict get? $Fdefaults %T%] $args [list name $name type %T% %F% [armour [uplevel 1 [list subst $value]]]]]
 
-		if {0 && ![dict exists $config tabindex]} {
+		if {![dict exists $config tabindex]} {
 		    variable tabindex
 		    dict set config tabindex [incr tabindex]
 		}
 
 		if {![dict exists $config id]} {
-		    if {[dict exists $config label]} {
-			dict set config id $name
-			set id $name
-		    }
-		} else {
-		    set id [dict get $config id]
+		    dict set config id $name
 		}
+		set id [dict get $config id]
 
 		# get sub-attributes of form "{subel attr} value"
 		set sattr {label {} legend {}}
@@ -413,14 +451,20 @@ namespace eval Form {
 		    }
 		}
 
-		set result "<[attr input [Dict subset $config $@AA]]>"
+		set result "<[attr input [Dict subset $config $%A%A]]>"
 
-		if {[dict exists $config label]} {
-		    set label [dict get $config label]
-		    return [<label> for $id {*}[dict get $sattr label] "$label $result"]
+		set label [dict get? $config label]
+		variable tabular
+		if {$tabular} {
+		    if {$label eq ""} {
+			set label $name
+		    }
+		    return "[<td> class label [<label> for $id $label]] [<td> class field $result]"
+		} elseif {$label ne ""} {
+		    return "[<label> for $id $label] $result"
 		} elseif {[dict exists $config legend]} {
 		    set legend [dict get $config legend]
-		    return "[<span> {*}[dict get $sattr legend] $legend]$result"
+		    return "[<span> {*}[dict get $sattr legend] $legend] $result"
 		} else {
 		    return $result
 		}
@@ -449,16 +493,16 @@ namespace eval Form {
 
     # <radioset> <checkset> <radio> <checkbox>
     foreach type {radio check} sub {"" box} {
-	eval [string map [list @T $type @S $sub] {
-	    proc <@Tset> {name args} {
+	eval [string map [list %T% $type %S% $sub] {
+	    proc <%T%set> {name args} {
 		variable Fdefaults
-		set rsconfig [dict merge [dict get? $Fdefaults @T] [lrange $args 0 end-1] [list name $name type @T]]
+		set rsconfig [dict merge [dict get? $Fdefaults %T%] [lrange $args 0 end-1] [list name $name type %T%]]
 		set boxes [lindex $args end]
 		set result {}
 		
 		set accum ""
 		foreach {content value} $boxes {
-		    set config [dict merge [dict get? $Fdefaults @T@S] $rsconfig]
+		    set config [dict merge [dict get? $Fdefaults %T%%S%] $rsconfig]
 		    if {[string match +* $content]} {
 			dict set config checked 1
 			set content [string trim $content +]
@@ -466,7 +510,7 @@ namespace eval Form {
 			catch {dict unset config checked}
 		    }
 		    dict set config value $value
-		    lappend result [uplevel 1 [list <@T@S> $name {*}$config $content]]
+		    lappend result [uplevel 1 [list <%T%%S%> $name {*}$config $content]]
 		    set accum ""
 		}
 		if {[dict exists $rsconfig vertical]
@@ -498,8 +542,8 @@ namespace eval Form {
 	    }
 	}]
 	
-	eval [string map [list @T $type$sub] {
-	    proc <@T> {name args} {
+	eval [string map [list %T% $type$sub] {
+	    proc <%T%> {name args} {
 		if {[llength $args] % 2} {
 		    set content [lindex $args end]
 		    set args [lrange $args 0 end-1]
@@ -508,7 +552,7 @@ namespace eval Form {
 		}
 		variable boxA
 		variable Fdefaults
-		set config [dict merge $args [list name $name type @T] [dict get? $Fdefaults @T]]
+		set config [dict merge $args [list name $name type %T%] [dict get? $Fdefaults %T%]]
 
 		set content [uplevel 1 [list subst $content]]
 		if {![dict exists $config label] && $content ne ""} {
@@ -516,29 +560,25 @@ namespace eval Form {
 		    set content ""
 		}
 
-		if {0 && ![dict exists $config tabindex]} {
+		if {![dict exists $config tabindex]} {
 		    variable tabindex
 		    dict set config tabindex [incr tabindex]
 		}
 
-		set id {}
 		if {![dict exists $config id]} {
-		    if {0 && [dict exists $config label]} {
-			dict set config id $name
-			set id [list for $name]
-		    }
-		} else {
-		    set id [list for [dict get $config id]]
+		    variable uniqID
+		    dict set config id F[incr uniqID]
 		}
+		set id [dict get $config id]
 		set result "<[attr input [Dict subset $config $boxA]]>$content"
 		if {[dict exists $config label]
 		    && [dict get $config label] ne ""
 		} {
 		    set label [dict get $config label]
 		    if {[dict exists $config title]} {
-			return [<label> {*}$id title [dict get $config title] "$label $result"]
+			return "[<label> for $id title [dict get $config title] $label] $result"
 		    } else {
-			return [<label> {*}$id "$label $result"]
+			return "[<label> for $id $label] $result"
 		    }
 		} else {
 		    return $result
@@ -607,16 +647,15 @@ Form init
 namespace import ::Form::<*>
 
 if {[info exists argv0] && ($argv0 eq [info script])} {
-    #Debug on form 10
     Form default textarea rows 8 cols 60
 
     puts "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head></head><body>"
     puts [<form> xxx action http:moop.html {
 	[<p> "This is a form to enter your account details"]
-	[<fieldset> details vertical 1 title "Personal Details" {
+	[<fieldset> details vertical 1 title "Account Details" {
 	    [<legend> "Account Details"]
-	    [<text> user legend "User name" title "Your preferred username (only letters, numbers and spaces)"]
-	    [<text> email title "Your email address" moop]
+	    [<text> user label "User name" title "Your preferred username (only letters, numbers and spaces)"]
+	    [<text> email label "Email Address" title "Your email address" moop]
 	    [<hidden> hidden moop]
 	}]
 	[<fieldset> passwords maxlength 16 size 16 {
@@ -641,10 +680,8 @@ if {[info exists argv0] && ($argv0 eq [info script])} {
 	    [<option> value moop1 label moop1 value 1 "Petit"]
 	    [<option> label moop2 value moop2 value 2 "Massive"]
 	}]
-	[<fieldset> personal {
-	    [<label> "Personal Information"]
-	    [<text> fullname title "Full name to be used in email."]
-	    [<text> phone title "Phone number for official contact"]
+	[<fieldset> personal tabular 1 legend "Personal Information" {
+	    [<text> fullname label "full name" title "Full name to be used in email."] [<text> phone label phone title "Phone number for official contact"]
 	}]
 	[<p> "When you create the account instructions will be emailed to you.  Make sure your email address is correct."]
 	[<textarea> te compact 1 {
@@ -654,8 +691,7 @@ if {[info exists argv0] && ($argv0 eq [info script])} {
 	<br>[<submit> submit "Create New Account"]
 
 	[<br>]
-	[<fieldset> permissions {
-	    [<legend> Permissions]
+	[<fieldset> permissions -legend Permissions {
 	    [<fieldset> gpermF style "float:left" title "Group Permissions." {
 		[<legend> Group]
 		[<checkbox> gperms title "Can group members read this page?" value 1 checked 1 read]
@@ -686,11 +722,12 @@ if {[info exists argv0] && ($argv0 eq [info script])} {
 	}]]
     }]
     puts "<hr />"
-    puts [<form> yyy action http:moop.html {
+    set body {
 	[<fieldset> fsearch {
 	    [<submit> submit "Search"]
 	    [<text> kw title "Search Text"]
-	    [<radioset> scope title "Search scope" {
+	    [<br> clear both]
+	    [<radioset> scope {fieldset style} "float:left" legend "Search scope" {
 		+site 0
 		section 1
 	    }]
@@ -706,15 +743,34 @@ if {[info exists argv0] && ($argv0 eq [info script])} {
 		[<option> month value "last month"]
 		[<option> year value "last year"]
 	    }]
-	    [<select> sort style "float:left" title "Sort By" {
+	    [<select> sort {fieldset style} "float:left" legend "Sort By" {
 		[<option> title value title]
 		[<option> author value author]
 	    }]
-	    [<selectset> sort1 style "float:left" title "Sort By" {
+	}]
+	[<fieldset> sr1 {
+	    [<radioset> scope1 label "Search scope" {
+		+site 0
+		section 1
+	    }]
+	    [<select> newer1 label "Newer Than" {
+		[<option> week value "last week"]
+		[<option> fortnight value "last fortnight"]
+		[<option> month value "last month"]
+		[<option> year value "last year"]
+	    }]
+	    [<select> older1 label "Older Than" {
+		[<option> week value "last week"]
+		[<option> fortnight value "last fortnight"]
+		[<option> month value "last month"]
+		[<option> year value "last year"]
+	    }]
+	    [<selectset> sort1 label "Sort By" {
 		title
 		author
 	    }]
 	}]
-    }]
+    }
+    puts [<form> yyy action http:moop.html $body]
     puts "</body>\n</html>"
 }
