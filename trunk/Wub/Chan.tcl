1ede623940db121703632b1894ed544fee54946e
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2009-12-02 05:19:19 +0000

    * added a check for FD leaks

--- trunk/Wub/Chan.tcl
+++ trunk/Wub/Chan.tcl
@@ -346,7 +346,7 @@ class create Socket {
 	} else {
 	    Debug.connections {$ip connected from port $port ([dict size $x] connections) / [llength [chan names]] open fds}
 	}
-	if {[length [chan names]] > 400} {
+	if {[llength [chan names]] > 400} {
 	    Debug.error {Waaaay too many open fds:  [chan names]}
 	}
     }
