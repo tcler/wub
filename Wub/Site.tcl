0966d0c877a9c3c4a90faa338eb78a8fb691861d
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-07-10 02:23:22 +0000

    * stop error on non-existent config file

--- Wub/Site.tcl
+++ Wub/Site.tcl
@@ -340,7 +340,7 @@ namespace eval ::Site {
 	puts stderr "CFG1: ($C)"
 	if {[dict exists $C Wub config]} {
 	    set phase "Site user configuration"	;# move to site config files phase
-	    if {[file exists [dict get $C Wub config]] {
+	    if {[file exists [dict get $C Wub config]} {
 		config aggregate [Config create user file [dict get $C Wub config]]
 		set C [config extract]	;# extract configuration values
 		user destroy
