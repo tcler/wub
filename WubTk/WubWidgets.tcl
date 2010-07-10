6b3a35ed0eaf27bad1eea9b20340f7741caa6f6e
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-07-10 04:00:25 +0000

    * added variable behaviour to Text widget

--- WubTk/WubWidgets.tcl
+++ WubTk/WubWidgets.tcl
@@ -259,7 +259,7 @@ namespace eval ::WubWidgets {
 	    }
 	    
 	    my reset
-	    return [<textarea> [my widget] id $id {*}$disabled style [my style] rows [my cget -height] cols [my cget -width] $val]
+	    return [<textarea> [my widget] id $id {*}$class {*}$disabled style [my style] rows [my cget -height] cols [my cget -width] $val]
 	}
 	
 	superclass ::WubWidgets::widget
