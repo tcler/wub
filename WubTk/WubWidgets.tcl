8d8a194d25e190e2a0a29c4f8441d2bc353ed556
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-07-22 13:42:31 +0000

    * upload widget

--- WubTk/WubWidgets.tcl
+++ WubTk/WubWidgets.tcl
@@ -1221,7 +1221,7 @@ namespace eval ::WubWidgets {
 	    set id [my id $id]
 	    variable connection
 	    set content [$connection layout form_$id enctype multipart/form-data class upload_form [subst {
-		file $id {}
+		file $id upload
 		submit send_$id Upload
 		hidden id [my widget]
 		hidden _op_ upload
