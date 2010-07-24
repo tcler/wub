3f780c2144485533e5d9a20dcf13d5f4d69d11e0
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-07-24 14:11:15 +0000

    * made theme find new-style themes

--- trunk/Domains/jQ/jQ.tcl
+++ trunk/Domains/jQ/jQ.tcl
@@ -171,7 +171,7 @@ namespace eval ::jQ {
 
     proc theme {r theme} {
 	variable mount; variable root
-	if {[file exists [file join $root $theme jquery.ui.all.css]]} {
+	if {[file exists [file join $root themes $theme jquery.ui.all.css]]} {
 	    return [Html style $r [join [list $mount themes $theme jquery.ui.all.css] /]]
 	} elseif {[file exists [file join $root themes $theme ui.all.css]]} {
 	    return [Html style $r [join [list $mount themes $theme ui.all.css] /]]
