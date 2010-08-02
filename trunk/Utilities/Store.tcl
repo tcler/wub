91dd5f49855b567dc5335f4038f26cea87aa2b40
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-08-02 14:18:56 +0000

    * added a few more facilities

--- trunk/Utilities/Store.tcl
+++ trunk/Utilities/Store.tcl
@@ -1,13 +1,19 @@
-# Store - a simple wrapper around TDBC
+# Store - a simple wrapper around TDBC providing a store
+
 package require tdbc
-package require Debug
-Debug define store 10
+if {[catch {package require Debug}]} {
+    #proc Debug.store {msg} {puts stderr "STORE: $msg"}
+    proc Debug.store {msg} {}
+} else {
+    Debug define store 10
+}
 
 package provide Store 1.0
 
 oo::class create Store {
     # stmt - evaluate tdbc statement
     # caches prepared statements
+    # returns resultset as dict
     method stmt {stmt args} {
 	if {[llength $args] == 1} {
 	    set args [lindex $args 0]
@@ -29,6 +35,7 @@ oo::class create Store {
 
     # stmtL - evaluate tdbc statement
     # caches prepared statements
+    # returns resultset as list
     method stmtL {stmt args} {
 	if {[llength $args] == 1} {
 	    set args [lindex $args 0]
@@ -47,13 +54,110 @@ oo::class create Store {
 	return $result
     }
 
+    # change a given tuple by id
+    method change {args} {
+	if {[llength $args] == 1} {
+	    set args [lindex $args 0]
+	}
+	if {[llength $args]%2} {
+	    set args [lassign $args table]
+	} else {
+	    variable primary; set table $primary
+	}
+	if {![dict exists $args id]} {
+	    error "changeset must contain id"
+	}
+
+	set updates {}
+	foreach n [dict keys $args] {
+	    if {$n ni {id}} {
+		lappend updates "$n=:$n"
+	    }
+	}
+	return [my stmt "UPDATE $table SET [join $updates ,] WHERE id=:id" {*}$args]
+    }
+
+    # insert a tuple, return its id
+    method insert {args} {
+	if {[llength $args] == 1} {
+	    set args [lindex $args 0]
+	}
+	if {[llength $args]%2} {
+	    set args [lassign $args table]
+	} else {
+	    variable primary; set table $primary
+	}
+	set vs {}
+	dict for {n v} $args {
+	    if {$n ne "id"} {
+		lappend vs :$n
+	    } else {
+		dict unset dict $n
+	    }
+	}
+
+	return [my stmt "INSERT INTO $table ([join [dict keys $args] ,]) VALUES ([join $vs,])" $args]
+    }
+
+    # matching tuples
+    method match {args} {
+	if {[llength $args] == 1} {
+	    set args [lindex $args 0]
+	}
+	if {[llength $args]%2} {
+	    set args [lassign $args table]
+	} else {
+	    variable primary; set table $primary
+	}
+
+	set sel {}
+	foreach n [dict keys $args] {
+	    lappend sel $n=:$n
+	}
+	return [my stmt "SELECT * FROM $table WHERE ([join $sel ,])" $args]
+    }
+
+    # return tuples by a single field match
+    method by {name value {table ""}} {
+	if {$table eq ""} {
+	    variable primary; set table $primary
+	}
+	return [my stmt "SELECT * FROM $table WHERE ($name=:value)" value $value]
+    }
+
+    # return only one tuple by a single field match
+    method oneby {name value {table ""}} {
+	if {$table eq ""} {
+	    variable primary; set table $primary
+	}
+	set result [my by $table $name $value]
+	if {[llength $result] > 1} {
+	    error "one $name '$value' returned [llength $result] results: ($result)"
+	}
+	return [lindex $result 0]
+    }
+
+    # maxid of given table/primary
+    method maxid {{table ""}} {
+	if {$table eq ""} {
+	    variable primary; set table $primary
+	}
+	set result [my stmtL "SELECT MAX(id) FROM $table"]
+	if {$result eq "{{}}"} {
+	    return 0
+	} else {
+	    return $result
+	}
+    }
+
     constructor {args} {
 	Debug.store {Creating Store [self] $args}
 	variable tdbc sqlite3	;# TDBC backend
 	variable db ""		;# already open db
 	variable file ""	;# or db file
 	variable opts {}
-	variable schema {}
+	variable schema {}	;# schema for empty dbs
+	variable primary ""	;# primary table of interest
 	variable {*}$args
 	variable stmts {}
 
@@ -70,7 +174,9 @@ oo::class create Store {
 	}
 	oo::objdefine [self] forward db $db
 
-	if {![llength [db tables tuples]]} {
+	if {$primary ne ""
+	    && $primary ni [db tables]
+	} {
 	    # we don't have any tables - apply schema
 	    if {$schema eq ""} {
 		error "Must provide a schema or an initialized db"
