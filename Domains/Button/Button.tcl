7a0fad7bcfcd7027451d54c076df6bcbed3b88ce
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-08-11 21:22:34 +0000

    * added Button domain for generating coloured SVN

---
+++ Domains/Button/Button.tcl
@@ -0,0 +1,87 @@
+# Button.tcl - generate a lovely custom aqua-style svg button
+package require Debug
+Debug define button 10
+package require fileutil
+package require Color
+
+package provide Button 1.0
+
+set ::API(Domains/Button) {
+    {Pretty Buttons for Wub}
+}
+
+set ::Button_dir [file normalize [file dirname [info script]]]
+
+oo::class create Button {
+    method rehue {svn newhue} {
+	# scan svn for colors
+	set colors [lsort -nocase -unique [regexp -inline -all {(#[a-fA-F0-9]{6})} $svn]]
+	set cnt 0; set sum 0
+	set hueof {}
+	foreach c $colors {
+	    set hsv [Color webToHsv $c]
+	    lassign $hsv hue
+	    if {$hue} {
+		dict set hueof $c $hue
+		incr sum $hue
+		incr cnt
+	    }
+	}
+	set newhue [expr {$newhue - $sum/$cnt}]	;# calculate color shift
+
+	set map {}
+	foreach c [dict keys $hueof] {
+	    dict set map $c [Color rehueweb $c $newhue]
+	}
+
+	return [string map $map $svn]	;# substitute colours
+    }
+
+    method getFile {name} {
+	variable files
+	if {![info exists files($name)]} {
+	    set files($name) [::fileutil::cat -- $name]
+	}
+	return $files($name)
+    }
+
+    method / {r args} {
+	set opts {}
+	foreach n {width height} {
+	    set v [dict args.$n?]
+	    if {$v ne ""} {
+		lappend opts $n ${v}px
+	    }
+	}
+	set fn [file join $::Button_dir [string map [list [file separator] /] [dict get $r -extra]]]
+	set hue [dict args.hue?]
+	if {$hue eq ""} {
+	    variable expires
+	    set r [Http Cache $r $expires]
+	    return [Http CacheableFile $r $fn image/svg+xml]
+	}
+
+	set svn [my getFile $fn]
+	if {[string is integer -strict $hue]} {
+	    set svn [my rehue $svn #$hue]
+	} elseif {[string match #* $hue]} {
+	    lassign [Color webToHsv $hue] hue
+	    set svn [my rehue $svn $hue]
+	} elseif {$hue ne ""} {
+	    lassign [Color webToHsv [Color nameToWeb $hue]] hue
+	}
+	set svn [my rehue $svn $hue]
+
+	variable expires
+	set r [Http Cache $r $expires]
+	return [Http Ok $r $svn image/svg+xml]
+    }
+
+    superclass Direct
+    constructor {args} {
+	variable expires "next week"
+	variable {*}[Site var? Button]	;# allow .ini file to modify defaults
+	variable {*}$args
+	next? {*}$args
+    }
+}
