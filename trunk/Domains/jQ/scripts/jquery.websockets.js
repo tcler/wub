29e945bb7b462331001b6f7b345f70f58f49e818
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2010-08-13 23:52:43 +0000

    * mucking about

--- trunk/Domains/jQ/scripts/jquery.websockets.js
+++ trunk/Domains/jQ/scripts/jquery.websockets.js
@@ -24,20 +24,23 @@
 		$(ws)
 		    .bind('open', $.websocketSettings.open)
 		    .bind('close', $.websocketSettings.close)
-		    .bind('message', $.websocketSettings.message)
-		    .bind('message', function(e){
-			    var m = $.evalJSON(e.originalEvent.data);
-			    var h = $.websocketSettings.events[m.type];
-			    if (h) h.call(this, m);
-			});
-		ws._settings = $.extend($.websocketSettings, s);
-		ws._send = ws.send;
-		ws.send = function(type, data) {
-		    var m = {type: type};
-		    m = $.extend(true, m, $.extend(true, {}, $.websocketSettings.options, m));
-		    if (data) m['data'] = data;
-		    return this._send($.toJSON(m));
-		}
+		    .bind('message', $.websocketSettings.message);
+		    /*.bind('message', function(e){
+		      var m = $.evalJSON(e.originalEvent.data);
+		      var h = $.websocketSettings.events[m.type];
+		      if (h) h.call(this, m);
+		      });
+
+		      ws._settings = $.extend($.websocketSettings, s);
+		      ws._send = ws.send;
+		      ws._send = function(type, data) {
+		      var m = {type: type};
+		      m = $.extend(true, m, $.extend(true, {}, $.websocketSettings.options, m));
+		      if (data) m['data'] = data;
+		      //return this._send($.toJSON(m));
+		      return this._send(m);
+		      }
+		    */
 		$(window).unload(function(){ ws.close(); ws = null });
 		return ws;
 	    }
