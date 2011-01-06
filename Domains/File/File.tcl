7e4f40328be39f8fc666b55fa2321deb03897d0f
mcccol <mcccol@df210599-c42c-0410-9b26-a5595cc827ef>
2011-01-06 02:19:29 +0000

    * whitespace cosmetics

--- Domains/File/File.tcl
+++ Domains/File/File.tcl
@@ -82,7 +82,7 @@ class create ::File {
 	    # this is a file name like '.tml'
 	    return [Http NotFound $r "<p>File '$suffix' has illegal name.</p>"]
 	}
-	
+
 	if {![file exists $path]} {
 	    # if the file doesn't exist, say so.
 	    return [Http NotFound $r "<p>File '$suffix' doesn't exist</p>"]
@@ -99,7 +99,7 @@ class create ::File {
 		return [Http NotModified $r]
 	    }
 	}
-	
+
 	Debug.file {FILE DISPATCH '$path' $r}
 	Debug.file {Found file '$path' of type [file type $path]}
 	set count 20
@@ -124,7 +124,7 @@ class create ::File {
 			return [Http CacheableFile $r $path]
 		    }
 		}
-		
+
 		directory {
 		    # if a directory reference doesn't end in /, redirect.
 		    Debug.file {redirecting path:$path, suffix:$suffix, -path:[dict get $r -path]}
@@ -135,7 +135,7 @@ class create ::File {
 		    } else {
 			# TODO do something to return the whole dir in one hit
 		    }
-		    
+
 		    # try to return an index file's contents in lieue of the directory
 		    if {$indexfile ne ""} {
 			set indices [glob -nocomplain -tails -directory $path $indexfile]
@@ -152,7 +152,7 @@ class create ::File {
 			return [Http CacheableContent [Http Cache $r $expires] [clock seconds]]
 		    }
 		}
-		
+
 		default {
 		    set r [Http Cache $r $expires]
 		    return [Http NotFound $r "<p>File '$suffix' is of illegal type [file type $path]</p>"]
