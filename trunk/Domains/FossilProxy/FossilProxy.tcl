10af02765df20c4afb7aa5c57eac91f5893e6b0f
jos.decoster <jos.decoster@df210599-c42c-0410-9b26-a5595cc827ef>
2011-04-11 13:42:11 +0000

    FossilProxy: inherit from Direct iso mixin

--- trunk/Domains/FossilProxy/FossilProxy.tcl
+++ trunk/Domains/FossilProxy/FossilProxy.tcl
@@ -12,9 +12,12 @@ set ::API(Domains/FossilProxy) {
     fossil_dir {Directory where fossil repositories are located. The proxy will work for all repositories in this directory which are named *.fossil, where the basename of the repository is part of the URL. Mandatory.}
     fossil_command {Path to fossil command. Default is 'fossil'}
     prefix {Path where fossil repositories are mounted in the URL. Mounted in root is the default.}
+    repositories_list_body {HTML body used to show list of available repositories. %REPOS% is replace by an unordered list of repositories. Default is empty.}
 }
 
 oo::class create FossilProxy {
+
+    superclass Direct
 
     method strip_prefix { path } {
  	variable prefix
@@ -24,7 +27,132 @@ oo::class create FossilProxy {
 	return $path
     }
 
-    method do { r } { 
+    method /direct/user/togglepriv/post {r R U P} {
+ 	variable fossil_dir
+ 	variable fossil_command
+ 	if {[catch {exec $fossil_command user capabilities $U -R [file join $fossil_dir $R]} Res]} {
+ 	    error $Res
+ 	}
+ 	set idx [string first $P $Res]
+ 	if {$idx >= 0} {
+ 	    set Res [string replace $Res $idx $idx]
+ 	    set msg "Revoked $P from user $U for repository $R: $Res"
+ 	} else {
+ 	    append Res $P
+ 	    set msg "Granted $P to user $U for repository $R: $Res"
+ 	}
+ 	if {[catch {exec $fossil_command user capabilities $U $Res -R [file join $fossil_dir $R]} Res]} {
+ 	    error $Res
+ 	}
+ 	return [Http NoCache [Http Ok $r $msg]]
+    }
+
+    method /direct/user/create/post {r R U C P} {
+ 	variable fossil_dir
+ 	variable fossil_command
+ 	if {[catch {exec $fossil_command user new $U $C $P -R [file join $fossil_dir $R]} Res]} {
+ 	    error $Res
+ 	}
+ 	return [Http NoCache [Http Ok $r $Res]]
+    }
+
+    method /direct/user/password/post { r R U P} {
+ 	variable fossil_dir
+ 	variable fossil_command
+ 	if {[catch {exec $fossil_command user password $U $P -R [file join $fossil_dir $R]} Res]} {
+ 	    error $Res
+ 	}
+ 	return [Http NoCache [Http Ok $r $Res]]
+    }
+
+    method /direct/privs {r} {
+	variable prefix
+	variable fossil_dir
+	variable fossil_command
+	set uidl {}
+	set C ""
+	set repoid 0
+	set privid 0
+	set fnml [lsort -dictionary [glob -nocomplain -tails -dir $fossil_dir *.fossil]]
+	foreach fnm $fnml {
+	    append C [<a> name repo$repoid [<h2> [file rootname $fnm]]]
+	    set rnm [file join $fossil_dir $fnm]
+	    if {[catch {exec $fossil_command user list -R $rnm} R]} {
+		error $R
+	    }
+	    unset -nocomplain kprivs
+	    unset -nocomplain privs
+	    unset -nocomplain uidl
+	    foreach l [split $R \n] {
+		set idx [string first " " $l]
+		set uid [string trim [string range $l 0 $idx]]
+		set contact [string trim [string range $l $idx end]]
+		lappend uidl [list $uid $contact]
+		if {[catch {exec $fossil_command user capabilities -R $rnm $uid} P]} {
+		    error $P
+		}
+		foreach p [split $P {}] {
+		    set kprivs($p) 1
+		    set privs($uid,$p) 1
+		}
+	    }
+	    set data {}
+	    foreach l $uidl {
+		lassign $l uid contact
+		set l [list uid $uid contact $contact]
+		foreach p [lsort -dictionary [array names kprivs]] {
+		    #lappend l $p "<input id='$privid' type='checkbox' OnClick='FossilProxy.togglepriv($privid, \"$fnm\", \"$uid\", \"$p\")' [expr {[info exists privs($uid,$p)]?"checked":""}]>"
+		    lappend l $p [expr {[info exists privs($uid,$p)]?"X":"-"}]
+		    incr privid
+		}
+		lappend data [incr i] $l
+		append C "  </tr>\n"
+	    }
+	    append C [Report html $data headers [list uid contact {*}[lsort -dictionary [array names kprivs]]] class tablesorter sortable 0 evenodd 0 htitle ""]
+	    incr repoid
+	}
+	set T [<h1> "Repository privileges"]\n
+	append T <ul>\n
+	set repoid 0
+	foreach fnm $fnml {
+	    append T [<li> [<a> href #repo$repoid [file rootname $fnm]]]\n
+	    incr repoid
+	}
+	append T </ul>\n
+	append T $C
+	set r [jQ tablesorter $r table]
+	dict set r -content $T
+	dict set r content-type x-text/html-fragment
+	dict set r -title "Repository privileges"
+	return [Http NoCache [Http Ok $r]]
+    }
+
+    method list_repos {r} {
+	variable prefix
+	variable fossil_dir
+	variable repositories_list_body
+	set C "<ul>\n"
+	foreach fnm [lsort -dictionary [glob -nocomplain -tails -dir $fossil_dir *.fossil]] {
+	    append C [<li> [<a> href $prefix/[file rootname $fnm] [file rootname $fnm]]]\n
+	}
+	append C "</ul>\n"
+	return [Http NoCache [Http Ok $r [regsub {%REPOS%} $repositories_list_body $C]]]
+    }
+
+    method do {r} {
+	variable prefix
+	switch -glob --[dict get $r -path] {
+	    "$prefix/direct/privs*" -
+	    "$prefix/direct/user*" {
+		return [next $r]
+	    }
+	    default {
+		return [my fossil_http $r]
+	    }
+	}
+    }
+
+    method fossil_http { r } { 
 
 	variable fnmid
 	variable prefix
@@ -38,6 +166,9 @@ oo::class create FossilProxy {
 	} else {
 	    lassign [dict get $r -header] meth url ver
 	    set url [my strip_prefix $url]
+	    if {$url in {{} {/}}} {
+		return [my list_repos $r]
+	    }
 	    set fr "$meth $url $ver\n"
 	}
 
