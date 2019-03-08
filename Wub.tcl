83e59fe11e5bdb62ac7d5d81a6b288160524acb3
Jianhong.Yin <yin-jianhong@163.com>
2019-03-08 18:26:52 +0800

    remove Wub/Application.tcl
    just use tclsh Wub.tcl
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- Wub.tcl
+++ Wub.tcl
@@ -3,7 +3,14 @@ lappend auto_path [file dirname [info script]]
 package require Site
 package provide Wub 5.0
 
-Site init home [file normalize [file dirname [info script]]] config site.config debug 10 {*}$argv
+# Initialize Site
+set siteconf site.config
+set conffile [lindex $argv 0]
+if {[file exists $conffile]} {
+	set siteconf $conffile
+	set argv [lrange $argv 1 end]
+}
+Site init home [file normalize [file dirname [info script]]] config $siteconf debug 10 {*}$argv
 
 Site start	;# start server(s)
 
